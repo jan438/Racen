36cb0ab14eaf35deed157a83071b1a735250f811
jan438 <jan438@users.noreply.github.com>
2025-04-19 14:59:31 +0200

    format code

--- Formule1.py
+++ Formule1.py
@@ -120,10 +120,10 @@ for i in range(count):
     d.add(transform_svg("Logos/" + formule1data[i][1] + ".svg", logox - float(formule1data[i][10]) + col * colwidth, logoy - float(formule1data[i][11]) + row * rowheight, float(formule1data[i][12]), float(formule1data[i][13])))
     #land1 5
     landcode = formule1data[i][5]
-    d.add(transform_svg(lookupflag(landcode), col * colwidth + halfcolwidth + flagx, row * rowheight + 90, 0.3 ,0.3))
+    d.add(transform_svg(lookupflag(landcode), col * colwidth + halfcolwidth + flagx, row * rowheight + 90, 0.3 , 0.3))
     #land2 9
     landcode = formule1data[i][9]
-    d.add(transform_svg(lookupflag(landcode), col * colwidth + flagx, row * rowheight + 90, 0.3 ,0.3))
+    d.add(transform_svg(lookupflag(landcode), col * colwidth + flagx, row * rowheight + 90, 0.3 , 0.3))
     col = col + 1
     if col == 2:
         col = 0
