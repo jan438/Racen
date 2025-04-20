6facf976d64ffe6a96cf9b8e1b8ce66e8b123e62
jan438 <jan438@users.noreply.github.com>
2025-04-20 11:04:58 +0200

    renderpm added

---
+++ renderPM.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import csv
+import math
+import unicodedata
+from pathlib import Path
+from datetime import datetime, date, timedelta
+from ics import Calendar, Event
+from pypdf import PdfReader, PdfWriter
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.pagesizes import LETTER, A4, landscape, portrait
+from reportlab.lib.units import inch
+from reportlab.lib.colors import blue, green, black, red, pink, gray, brown, purple, orange, yellow, white, lightgrey
+from reportlab.pdfbase import pdfmetrics  
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase.pdfmetrics import registerFontFamily
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, Image, Spacer, Frame
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.enums import TA_LEFT, TA_RIGHT, TA_CENTER
+from reportlab.graphics.shapes import *
+from reportlab.graphics import renderPDF
+from reportlab.graphics.shapes import *
+from reportlab.graphics import shapes
+from reportlab.graphics import widgetbase
+from reportlab.graphics.widgetbase import Widget
+from reportlab.graphics.widgets import signsandsymbols
+from reportlab.graphics.widgets.signsandsymbols import _Symbol
+from reportlab.graphics.charts.textlabels import Label
+from reportlab.graphics import renderPM
+from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
+from PIL import Image as PILImage
+
+d = Drawing(400, 200)
+d.add(Rect(50, 50, 300, 100, fillColor=colors.yellow))
+d.add(String(150,100, 'Hello World', fontSize=18, fillColor=colors.red))
+fillColor = colors.red
+renderPM.drawToFile(d, 'PDF/example1.png', 'PNG')
+
+key = input("Wait")
