026fa21cb73401a32afd1be8e6ea7940c789aa20
jan438 <jan438@users.noreply.github.com>
2025-04-24 10:39:47 +0200

    corners added

--- Formule1.py
+++ Formule1.py
@@ -114,6 +114,7 @@ for i in range(count):
     #land2 9
     landcode = formule1data[i][9]
     d.add(transform_svg(lookupflag(landcode), col * colwidth + flagx, row * rowheight + 90, 0.3 , 0.3))
+    d.add(upperrightcorner(col * colwidth, row * rowheight, 10.0, 2, colors.black))
     col = col + 1
     if col == 2:
         col = 0
