0267c0550b873a8129b777e583237cc585a3aa29
jan438 <jan438@users.noreply.github.com>
2025-04-27 10:29:33 +0200

    alpine mercedes added

--- Formule1.py
+++ Formule1.py
@@ -80,8 +80,8 @@ d.add(transform_svg("SVG/F1.svg", 297.5 - 60, 800, 1.1, 1.1))
 rowheight = 160
 colwidth = 297.5
 halfcolwidth = 148.75
-logox = 250
-logoy = 130
+logox = 246
+logoy = 125
 row = 0
 col = 0
 flagx = 115
