da6d904d40a776ded91f45cd958766e26856e2fc
jan438 <jan438@users.noreply.github.com>
2025-04-29 13:37:53 +0200

    read geojson file

--- circuits.py
+++ circuits.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import geojson
+import pprint as pp
 from reportlab.graphics import renderPDF
 from reportlab.pdfgen import canvas
 from svglib.svglib import svg2rlg
@@ -9,6 +11,9 @@ if sys.platform[0] == 'l':
 if sys.platform[0] == 'w':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/Racen"
 os.chdir(path)
+with open('Circuits/GeoJSON/sample.geojson', 'r') as file:
+    geojson_data = geojson.load(file)
+pp.pprint(dict(geojson_data))
 my_canvas = canvas.Canvas('PDF/svg_on_canvas.pdf')
 drawing = svg2rlg('SVG/F1.svg')
 renderPDF.draw(drawing, my_canvas, 0, 40)
