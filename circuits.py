da4bd4b690676515de1c62886bb78943addbf662
jan438 <jan438@users.noreply.github.com>
2025-04-29 09:20:28 +0200

    circuits py file add

---
+++ circuits.py
@@ -0,0 +1,17 @@
+# svg_on_canvas.py
+
+from reportlab.graphics import renderPDF
+from reportlab.pdfgen import canvas
+from svglib.svglib import svg2rlg
+
+def add_image(image_path):
+    my_canvas = canvas.Canvas('PDF/svg_on_canvas.pdf')
+    drawing = svg2rlg(image_path)
+    renderPDF.draw(drawing, my_canvas, 0, 40)
+    my_canvas.drawString(50, 30, 'My SVG Image')
+    my_canvas.save()
+
+if __name__ == '__main__':
+    image_path = 'SVG/F1.svg'
+    add_image(image_path)
+    key = input("Wait")
