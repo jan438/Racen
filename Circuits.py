141b13368d9c38d6e2e9f643f1e81310e5186790
jan438 <jan438@users.noreply.github.com>
2025-05-05 11:32:22 +0200

    iets kleiner

--- Circuits.py
+++ Circuits.py
@@ -98,7 +98,7 @@ GeoJSON_to_SVG(circuitsdata[0][0])
 for i in range(count):
     renderPDF.draw(transform_svg("SVG/" + circuitsdata[i][0] + ".svg", col * colwidth, row * rowheight, 0.2, 0.2), my_canvas, 0, 40)
     my_canvas.drawString(col * colwidth, row * rowheight + 27, circuitsdata[i][0])
-    renderPDF.draw(transform_svg("SVG/finishflag.svg", col * colwidth, row * rowheight, 0.4, 0.4), my_canvas, 0, 40)
+    renderPDF.draw(transform_svg("SVG/finishflag.svg", col * colwidth, row * rowheight, 0.3, 0.3), my_canvas, 0, 40)
     col += 1
     if col == colcount:
        row += 1
