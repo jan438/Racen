f389d5de470253dd04793bb5d40b171834a36009
jan438 <jan438@users.noreply.github.com>
2025-05-06 14:12:06 +0200

    code relocated

--- Circuits.py
+++ Circuits.py
@@ -22,6 +22,8 @@ def GeoJSON_to_SVG(circuitname):
                 path_data += f"{command}{x},{height - y} "
             path_data += "Z "
         return path_data.strip()
+    width = 500
+    height = 500
     with open("Data/" + circuitname + ".geojson", 'r') as file:
         geojson_data = geojson.load(file)
     features = geojson_data['features']
@@ -46,8 +48,6 @@ def GeoJSON_to_SVG(circuitname):
                 max_x = max(max_x, x)
                 min_y = min(min_y, y)
                 max_y = max(max_y, y)
-    width = 500
-    height = 500
     scale_x = width / (max_x - min_x)
     scale_y = height / (max_y - min_y)
     scale = (scale_x, scale_y)
