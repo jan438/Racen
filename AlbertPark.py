2afe289ebda7b3750af36bd9d29ee7dc2bc0a177
jan438 <jan438@users.noreply.github.com>
2025-05-08 14:11:10 +0200

    albertpark finish changed

--- AlbertPark.py
+++ AlbertPark.py
@@ -118,12 +118,12 @@ name_y = 25
 row = 0
 col = 0
 GeoJSON_to_SVG(circuitsdata[cx][0])
-graden = 4
-minuten = 32
-seconden = 27
+graden = 144
+minuten = 58
+seconden = 6
 richting = 'E'
 # pagesize=(595.27,841.89),
-# Zandvoort *** 52°23′20″N 4°32′27″E  4.54083333 52.388888889  "bbox": [ 4.538742, 52.384363, 4.553061, 52.391811 ] 
+# AlbertPark *** 37°50′59″S 144°58′6″E  4.54083333 52.388888889  "bbox": [ 4.538742, 52.384363, 4.553061, 52.391811 ] 
 # width = 4.553061 - 4.538742 = 0.014319  start_x 4.54083333 - 4.538742 = 0.0020913
 decimale_breedtegraad = dms_to_decimal(graden, minuten, seconden, richting)
 print(f"Decimale breedtegraad: {decimale_breedtegraad}")
