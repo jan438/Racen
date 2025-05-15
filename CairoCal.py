92e839e702bff8d8b3e231cd05e8b20d3d0a65e0
jan438 <jan438@users.noreply.github.com>
2025-05-15 13:22:54 +0200

    Cairi cal added

--- CairoCal.py
+++ CairoCal.py
@@ -9,11 +9,6 @@ import unicodedata
 import svgwrite
 from svgwrite import Drawing
 import cairosvg
-from reportlab.graphics import renderPDF
-from reportlab.pdfgen import canvas
-from reportlab.lib.units import inch, mm
-from reportlab.graphics.shapes import *
-from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
 
 def generate_calendar_svg(year=None, month=None, start_day=0, file_name="calendar.svg", as_text=False):
     output_dir = "SVG"
@@ -96,28 +91,11 @@ def convert_text_to_paths(svg_path):
     path_svg_path = svg_path
     cairosvg.svg2svg(url=svg_path, write_to=path_svg_path)
     print(f"Text converted to paths.")
-def scaleSVG(svgfile, scaling_factor):
-    svg_root = load_svg_file(svgfile)
-    svgRenderer = SvgRenderer(svgfile)
-    drawing = svgRenderer.render(svg_root)
-    scaling_x = scaling_factor
-    scaling_y = scaling_factor
-    drawing.width = drawing.minWidth() * scaling_x
-    drawing.height = drawing.height * scaling_y
-    drawing.scale(scaling_x, scaling_y)
-    return drawing
 
 if sys.platform[0] == 'l':
     path = '/home/jan/git/Racen'
 if sys.platform[0] == 'w':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/Racen"
 os.chdir(path)
-my_canvas = canvas.Canvas("PDF/Calendar2025.pdf")
-my_canvas.setFont("Helvetica", 25)
-my_canvas.setTitle("Calendar 2025")
-drawing = svg2rlg('SVG/F1.svg')
-renderPDF.draw(drawing, my_canvas, 100, 800)
-#file_path = generate_calendar_svg(2025, 5, 1, "May2025.svg", False)
-renderPDF.draw(scaleSVG("SVG/May2025.svg", 0.45), my_canvas, 75, 300)
-my_canvas.save()
+file_path = generate_calendar_svg(2025, 5, 1, "May2025.svg", False)
 key = input("Wait")
