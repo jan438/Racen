a650ae212350de15d7b507bdea0f9cf76138fc4b
jan438 <jan438@users.noreply.github.com>
2025-05-15 15:38:22 +0200

    flaf offset added

--- Calendar2025.py
+++ Calendar2025.py
@@ -45,11 +45,12 @@ leftmargin = 75
 bottommargin = 50
 colwidth = 200
 rowheight = 125
+flagoffset = 140
 for i in range(12):
     renderPDF.draw(scaleSVG("SVG/" + monthnames[11 - i] + ".svg", 0.25), my_canvas, leftmargin + col * colwidth, bottommargin + row * rowheight)
     if i == 7:
-        renderPDF.draw(scaleSVG("Flags/NL.svg", 0.25), my_canvas, leftmargin + col * colwidth, bottommargin + row * rowheight)
-        renderPDF.draw(scaleSVG("SVG/clock-two-thirty.svg", 0.025), my_canvas, leftmargin + 20 + col * colwidth, bottommargin + row * rowheight)
+        renderPDF.draw(scaleSVG("Flags/NL.svg", 0.25), my_canvas, leftmargin + flagoffset + col * colwidth, bottommargin + row * rowheight)
+        renderPDF.draw(scaleSVG("SVG/clock-two-thirty.svg", 0.025), my_canvas, leftmargin + flagoffset + 20 + col * colwidth, bottommargin + row * rowheight)
     col -= 1
     if col == -1:
         row += 1
