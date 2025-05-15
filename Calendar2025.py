6f701ead2619c17cc7a6657935e0d1fe96324214
jan438 <jan438@users.noreply.github.com>
2025-05-15 15:27:29 +0200

    clock added

--- Calendar2025.py
+++ Calendar2025.py
@@ -49,6 +49,7 @@ for i in range(12):
     renderPDF.draw(scaleSVG("SVG/" + monthnames[11 - i] + ".svg", 0.25), my_canvas, leftmargin + col * colwidth, bottommargin + row * rowheight)
     if i == 7:
         renderPDF.draw(scaleSVG("Flags/NL.svg", 0.25), my_canvas, leftmargin + col * colwidth, bottommargin + row * rowheight)
+        renderPDF.draw(scaleSVG("SVG/clock-two-thirty.svg", 0.025), my_canvas, leftmargin + 20 + col * colwidth, bottommargin + row * rowheight)
     col -= 1
     if col == -1:
         row += 1
