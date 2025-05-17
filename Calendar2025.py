9dc4e4ce5a0fc072fb247de2c13cf368d6b396dd
jan438 <jan438@users.noreply.github.com>
2025-05-17 13:39:55 +0200

    rect removed

--- Calendar2025.py
+++ Calendar2025.py
@@ -59,7 +59,8 @@ for i in range(12):
         linkx2 = linkx1 + 20
         linky2 = linky1 + 10
         linkarea = (linkx1, linky1, linkx2, linky2)
-        my_canvas.linkAbsolute("Find the Meaning of Life", "Meaning_of_life", linkarea, addtopage = 1, thickness = 1, color = colors.green)
+        #my_canvas.linkAbsolute("Find the Meaning of Life", "Meaning_of_life", linkarea, addtopage = 1, thickness = 1, color = colors.green)
+        my_canvas.linkAbsolute("Find the Meaning of Life", "Meaning_of_life", linkarea, addtopage = 1, thickness = 0, color = None)
     col -= 1
     if col == -1:
         row += 1
