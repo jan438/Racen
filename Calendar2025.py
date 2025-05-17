1420eed9011acda9225fd80167d7d306e7465275
jan438 <jan438@users.noreply.github.com>
2025-05-17 09:27:56 +0200

    racing car positioned

--- Calendar2025.py
+++ Calendar2025.py
@@ -43,12 +43,12 @@ leftmargin = 75
 bottommargin = 50
 colwidth = 200
 rowheight = 125
-flagoffset = 140
+flagoffset = 130
 for i in range(12):
     renderPDF.draw(scaleSVG("SVG/" + monthnames[11 - i] + ".svg", 0.25), my_canvas, leftmargin + col * colwidth, bottommargin + row * rowheight)
     if i == 4:
         renderPDF.draw(scaleSVG("Flags/NL.svg", 0.25), my_canvas, leftmargin + flagoffset + col * colwidth, bottommargin + row * rowheight)
-        renderPDF.draw(scaleSVG("SVG/clock-two-thirty.svg", 0.025), my_canvas, leftmargin + flagoffset + 20 + col * colwidth, bottommargin + row * rowheight)
+        renderPDF.draw(scaleSVG("SVG/racingcar.svg", 0.025), my_canvas, leftmargin + flagoffset - 20 + col * colwidth, bottommargin + row * rowheight + 6)
     col -= 1
     if col == -1:
         row += 1
