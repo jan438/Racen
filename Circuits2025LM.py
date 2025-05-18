9c6d920830bb2543ffcc2d22ca635f4458bbe91e
jan438 <jan438@users.noreply.github.com>
2025-05-18 08:39:52 +0200

    place for world

--- Circuits2025LM.py
+++ Circuits2025LM.py
@@ -115,7 +115,7 @@ colwidth = 115
 row = 0
 col = 0
 for i in range(count):
-    if i == 11:
+    if i == 11 or i == 13:
         col = col + 3
     [offset_x, offset_y] = GeoJSON_to_SVG(circuitsdata[i][1])
     circuit_x = col * colwidth
