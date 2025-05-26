1b8b9d54c1fe702aea7ded50d254d06915f0a567
jan438 <jan438@users.noreply.github.com>
2025-05-26 16:08:01 +0200

    row hieight added

--- Calendar2025.py
+++ Calendar2025.py
@@ -218,7 +218,7 @@ for i in range(len(raceevents)):
             my_canvas.arc(col * colwidth + 2.0, row * rowheight + 12.0, col * colwidth + 22.0, row * rowheight + 32.0, startAng = 90, extent = 90)
             my_canvas.line(col * colwidth + 2.0, row * rowheight + 2.0, col * colwidth + 2.0, row * rowheight + 22.0)
                  
-            my_canvas.line(col * colwidth, row * rowheight - 60, col * colwidth + 5.0, row * rowheight - 60)
+            my_canvas.line(col * colwidth, row * rowheight - 80, col * colwidth + 5.0, row * rowheight - 80)
             my_canvas.arc(col * colwidth + 120.0, row * rowheight - 80.0, col * colwidth + 140.0, row * rowheight - 60.0, startAng = 270, extent = 90)
             my_canvas.line(90.0, 20.0, 90.0, 40.0)
             
