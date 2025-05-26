d92b559b8b8dc1b1580ac255959ce610116b2a1a
jan438 <jan438@users.noreply.github.com>
2025-05-26 10:13:44 +0200

    alle arcen defined

--- Calendar2025.py
+++ Calendar2025.py
@@ -260,11 +260,9 @@ for i in range(len(raceevents)):
                 row = row - 1
                 if row < 0:
                     break
-my_canvas.arc(0.0, 0.0, 20.0, 20.0, startAng = 0, extent = 90)
-my_canvas.setFillColorRGB(1, 0.6, 0.8)
-p = my_canvas.beginPath()
-p.moveTo(0.2*inch, 0.2*inch)
-p.arcTo(inch, inch, 2.5*inch,2*inch, startAng=-30, extent=135)
-my_canvas.drawPath(p, fill=1, stroke=1)
+my_canvas.arc(0.0, 0.0, 20.0, 20.0, startAng = 90, extent = 90)
+my_canvas.arc(20.0, 20.0, 40.0, 40.0, startAng = 0, extent = 90)
+my_canvas.arc(40.0, 40.0, 60.0, 60.0, startAng = 180, extent = 90)
+my_canvas.arc(60.0, 60.0, 80.0, 80.0, startAng = 270, extent = 90)
 my_canvas.save()
 key = input("Wait")
