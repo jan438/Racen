00202cfae07bb1512fe86a2c503a1c24fc746a82
jan438 <jan438@users.noreply.github.com>
2025-06-11 15:34:09 +0200

    tail added

--- CircuitLM.py
+++ CircuitLM.py
@@ -104,8 +104,8 @@ def GeoJSON_to_SVG(geojsonfile, svgfile):
     print("Nearestpoinntstartfiniah", npointstartfinish, "Nearestpoints", startindices[0], startindices[1])
     if npointstartfinish > startindices[0] and npointstartfinish > startindices[1]:
         coords = tail(coords, npointstartfinish)
-        startindices[0] = startindices[0] + 50
-        startindices[1] = startindices[1] + 50
+        startindices[0] = startindices[0] + (len(coords) - npointstartfinish)
+        startindices[1] = startindices[1] + (len(coords) - npointstartfinish)
     svg_paths = []
     with open("SVG/" + svgfile + "LM.svg", 'w') as f:
         f.write(f'<svg width="{width}" height="{height}" xmlns="http://www.w3.org/2000/svg">\n')
