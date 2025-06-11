84f3bfeb02b798f397ede73d31365b66848fccbc
jan438 <jan438@users.noreply.github.com>
2025-06-11 13:46:42 +0200

    fetter clock

--- basicclock.py
+++ basicclock.py
@@ -94,7 +94,7 @@ def get_random_times(n, difficulty):
 
     times = []
     for i in range(n):
-        hr = 3
+        hr = 10
         mn = 0
         times.append('{}:{}'.format(hr,mn))
     return times
