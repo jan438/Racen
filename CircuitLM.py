33b98455569de4351f26ea554e3e1073c1c7bd91
jan438 <jan438@users.noreply.github.com>
2025-06-13 08:55:24 +0200

    if cx == 14 added

--- CircuitLM.py
+++ CircuitLM.py
@@ -111,14 +111,24 @@ def GeoJSON_to_SVG(geojsonfile, svgfile):
             geometry = feature['geometry']
             coords = geometry['coordinates']
             if geometry['type'] == 'LineString':
-                path = coordinates_to_path([coords[:startindices[0] + 1]], scale, translate)
-                f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="#fae44a"/>\n')
-                path = coordinates_to_path([coords[startindices[0] - 1:startindices[1] + 1]], scale, translate)
-                f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="#db4a25"/>\n')
-                path = coordinates_to_path([coords[startindices[1] - 1:startindices[2] + 1]], scale, translate)
-                f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="#00ffff"/>\n')
-                path = coordinates_to_path([coords[startindices[2] - 1:]], scale, translate)
-                f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="#1bce20"/>\n')
+                if cx == 14:
+                    path = coordinates_to_path([coords[:startindices[0] + 1]], scale, translate)
+                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="#fae44a"/>\n')
+                    path = coordinates_to_path([coords[startindices[0] - 1:startindices[1] + 1]], scale, translate)
+                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="#db4a25"/>\n')
+                    path = coordinates_to_path([coords[startindices[1] - 1:startindices[2] + 1]], scale, translate)
+                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="#00ffff"/>\n')
+                    path = coordinates_to_path([coords[startindices[2] - 1:]], scale, translate)
+                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="#1bce20"/>\n')
+                else:
+                    path = coordinates_to_path([coords[:startindices[0] + 1]], scale, translate)
+                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="#fae44a"/>\n')
+                    path = coordinates_to_path([coords[startindices[0] - 1:startindices[1] + 1]], scale, translate)
+                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="#db4a25"/>\n')
+                    path = coordinates_to_path([coords[startindices[1] - 1:startindices[2] + 1]], scale, translate)
+                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="#00ffff"/>\n')
+                    path = coordinates_to_path([coords[startindices[2] - 1:]], scale, translate)
+                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="#1bce20"/>\n')
         f.write('</svg>')    
     return [offset_x, offset_y]
 def transform_svg(svgfile, tx, ty, sx, sy): 
