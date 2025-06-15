b153e2fe7430e7a3cd8bb8f8fc4ce878854ef2ef
jan438 <jan438@users.noreply.github.com>
2025-06-15 18:49:42 +0200

    eno turn

--- CircuitLM.py
+++ CircuitLM.py
@@ -168,9 +168,9 @@ def GeoJSON_to_SVG(geojsonfile, svgfile):
                     idx2 = int(circuitsdata[cx][13])   # case enzo
                     idx3 = int(circuitsdata[cx][14])
                     path = coordinates_to_path([coords[idx1:idx2 + 1]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
-                    path = coordinates_to_path([coords[idx2 - 1:idx3 + 1]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
+                    path = coordinates_to_path([coords[idx2 - 1:idx3 + 1]], scale, translate)
+                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
                     path = coordinates_to_path([coords[idx3 - 1:]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
         f.write('</svg>')    
