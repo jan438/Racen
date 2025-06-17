be3bb04b06989178a06b29ada053ef3f7fd27ff7
jan438 <jan438@users.noreply.github.com>
2025-06-17 13:13:54 +0200

    marina bey

--- CircuitLM.py
+++ CircuitLM.py
@@ -210,12 +210,12 @@ def GeoJSON_to_SVG(geojsonfile, svgfile):
                     path = coordinates_to_path([coords[idx3 - 1:]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
                 elif cx == 12:
-                    path = coordinates_to_path([coords[:idx2 + 1]], scale, translate)
+                    path = coordinates_to_path([coords[idx2:idx1]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
-                    path = coordinates_to_path([coords[idx2 - 1:idx3 + 1]], scale, translate)
+                    path = coordinates_to_path([coords[idx3:idx2]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
-                    path = coordinates_to_path([coords[idx3 - 1:]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
+                    #path = coordinates_to_path([coords[idx3:]], scale, translate)
+                    #f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
                 else:
                     path = coordinates_to_path([coords[idx1:idx2 + 1]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
