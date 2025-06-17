ddca0c0e60218114fb3f3d6b8397445087a47369
jan438 <jan438@users.noreply.github.com>
2025-06-17 09:23:23 +0200

    jeddah in turn

--- CircuitLM.py
+++ CircuitLM.py
@@ -194,14 +194,8 @@ def GeoJSON_to_SVG(geojsonfile, svgfile):
                     idx1 = int(circuitsdata[cx][12])
                     idx2 = int(circuitsdata[cx][13])
                     idx3 = int(circuitsdata[cx][14])
-                    path = coordinates_to_path([coords[idx2 - 1:idx1 + 1]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
-                    path = coordinates_to_path([coords[idx3 - 1:idx2 + 1]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
-                    path = coordinates_to_path([coords[:idx3 + 1]], scale, translate)
+                    path = coordinates_to_path([coords[:idx2]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
-                    path = coordinates_to_path([coords[idx1 - 1:]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
                 else:
                     idx1 = int(circuitsdata[cx][12])
                     idx2 = int(circuitsdata[cx][13])   # clockwise start on zero
