f45e8965f210a73c518ff5e3e9216d154ec70d13
jan438 <jan438@users.noreply.github.com>
2025-06-18 10:53:33 +0200

    print extend

--- Circuits2025LM.py
+++ Circuits2025LM.py
@@ -122,8 +122,7 @@ def GeoJSON_to_Canvas(circuitindex):
     translate = (min_x, min_y)
     offset_x = (startfinish_x - min_x) * scale_x
     offset_y = (startfinish_y - min_y) * scale_y
-    #print("Scale", scale_x, scale_y, "Startfinish", startfinish_x, startfinish_y, "Offsetflag", offset_x, offset_y)
-    print(circuitsdata[circuitindex][0], "Offsetstart", round(offset_x, 3), round(offset_y, 3))
+    print(circuitsdata[circuitindex][0], str(circuitsdata[circuitindex][12]), "Offsetstart", round(offset_x, 3), round(offset_y, 3))
     return [offset_x, offset_y]
 def transform_svg(svgfile, tx, ty, sx, sy): 
     svg_root = load_svg_file(svgfile)
