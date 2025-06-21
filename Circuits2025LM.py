dd8ec09bffd6396fe5148faff08d15874a5ef1f0
jan438 <jan438@users.noreply.github.com>
2025-06-21 14:07:51 +0200

    wait replacement

--- Circuits2025LM.py
+++ Circuits2025LM.py
@@ -121,7 +121,7 @@ def GeoJSON_to_Canvas(circuitindex):
                 startfinish_y = point[1]
                 startfinish_offset_x = (startfinish_x - g_min_x) * scale_x
                 startfinish_offset_y = (startfinish_y - g_min_y) * scale_y
-                print("startfinish", round(startfinish_x), round(startfinish_offset_y))
+                #print("startfinish", round(startfinish_x), round(startfinish_offset_y))
             if i == sect1:
                 sect1_x = point[0]
                 sect1_y = point[1]
@@ -129,7 +129,7 @@ def GeoJSON_to_Canvas(circuitindex):
                 sect1_offset_y = (sect1_y - g_min_y) * scale_y
                 point2 = linestring[i + 1]
                 sect1_angle = get_angle(point, point2)
-                print("sect1", round(sect1_offset_x), round(sect1_offset_y))
+                #print("sect1", round(sect1_offset_x), round(sect1_offset_y))
             if i == sect2:
                 sect2_x = point[0]
                 sect2_y = point[1]
@@ -137,15 +137,18 @@ def GeoJSON_to_Canvas(circuitindex):
                 sect2_offset_y = (sect2_y - g_min_y) * scale_y
                 point2 = linestring[i + 1]
                 sect2_angle = get_angle(point, point2)
-                print("sect2", round(sect2_offset_x), round(sect2_offset_y))
+                #print("sect2", round(sect2_offset_x), round(sect2_offset_y))
     if circuitindex == 2:
+        print("startfinish", round(startfinish_x), round(startfinish_offset_y))
+        print("sect1", round(sect1_offset_x), round(sect1_offset_y))
+        print("sect2", round(sect2_offset_x), round(sect2_offset_y))
         startfinish_x = int(circuitsdata[circuitindex][15])
         startfinish_y = int(circuitsdata[circuitindex][16])
         sect1_offset_x = int(circuitsdata[circuitindex][18])
         sect1_offset_y = int(circuitsdata[circuitindex][19])
         sect2_offset_x = int(circuitsdata[circuitindex][21])
         sect2_offset_y = int(circuitsdata[circuitindex][22])
-    key = input("Wait")
+        key = input("Wait")
     return [startfinish_offset_x, startfinish_offset_y, sect1_offset_x, sect1_offset_y, sect1_angle, sect2_offset_x, sect2_offset_y, sect2_angle, length]
 def transform_svg(svgfile, tx, ty, sx, sy): 
     svg_root = load_svg_file(svgfile)
