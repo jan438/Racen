25657d3b8d7945a30b46328fccfb10f934818bc4
jan438 <jan438@users.noreply.github.com>
2025-06-24 10:42:19 +0200

    alle idexes changed albert

--- CircuitLM.py
+++ CircuitLM.py
@@ -126,7 +126,7 @@ def GeoJSON_to_SVG(geojsonfile, svgfile):
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
                     path = coordinates_to_path([coords[idx1:idx2 + 1]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
-                    path = coordinates_to_path([coords[startindices[2] - 1:startindices[1] + 1]], scale, translate)
+                    path = coordinates_to_path([coords[idx2 - 1:idx3 + 1]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
                     path = coordinates_to_path([coords[idx3 - 1:]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
