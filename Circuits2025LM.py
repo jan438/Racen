dec97a332d5bb38ba3a5150c7c1b76753e889498
jan438 <jan438@users.noreply.github.com>
2025-06-30 09:31:19 +0200

    print updated

--- Circuits2025LM.py
+++ Circuits2025LM.py
@@ -137,7 +137,7 @@ def GeoJSON_to_Canvas(circuitindex):
                 point2 = linestring[i + 1]
                 sect3_angle = get_angle(point, point2)
     if circuitindex == 10:
-        print("Todo", circuitsdata[circuitindex][0], round(startfinish_offset_x), round(startfinish_offset_y))
+        print("Todo", circuitsdata[circuitindex][0], round(startfinish_offset_x), round(startfinish_offset_y), circuitsdata[circuitindex][0], round(startfinish_offset_x), round(startfinish_offset_y), circuitsdata[circuitindex][0], round(startfinish_offset_x), round(startfinish_offset_y),)
     startfinish_offset_x = int(circuitsdata[circuitindex][15])
     startfinish_offset_y = int(circuitsdata[circuitindex][16])
     sect2_offset_x = int(circuitsdata[circuitindex][18])
