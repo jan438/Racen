87a4b2f97f8d38e191bad66f0df7631e2350a678
jan438 <jan438@users.noreply.github.com>
2025-07-13 11:35:38 +0200

    flaggoffset changed

--- Calendar2025.py
+++ Calendar2025.py
@@ -184,7 +184,7 @@ colwidth = 180
 rowheight = 160
 weekheight = 19
 flagoffset_x = 158
-flagoffset_y = 0
+flagoffset_y = 19
 linkx1 = 0
 linky1 = 0
 linkx2 = 10
@@ -217,7 +217,7 @@ for i in range(len(raceevents)):
         weeknr = round(day / 7 + 1)
         y_offset = (6 - weeknr) * weekheight + flagoffset_y
         if month == 4 or month == 5 or month == 7 or month == 9 or month == 10 or month == 12:
-            y_offset = y_offset + weekheight + flagoffset_y
+            y_offset = y_offset + weekheight
         renderPDF.draw(scaleSVG("SVG/formula-1color.svg", 0.028), my_canvas, leftmargin + flagoffset_x - 25 + col * colwidth, bottommargin + row * rowheight + y_offset)
         renderPDF.draw(scaleSVG("Flags/" + code + ".svg", 0.25), my_canvas, leftmargin + flagoffset_x + col * colwidth, bottommargin + row * rowheight + y_offset)
         linkx1 = leftmargin + flagoffset_x + col * colwidth
