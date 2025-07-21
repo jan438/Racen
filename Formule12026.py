17241526c064d4caf2b8eeed8ea9e92a53b5c6e1
jan438 <jan438@users.noreply.github.com>
2025-07-21 15:52:48 +0200

    totaal changed

--- Formule12026.py
+++ Formule12026.py
@@ -19,19 +19,19 @@ from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
 def processreport():
     merger = PdfWriter()
 
-    if os.path.isfile("PDF/Calendar2025.pdf"):
-        inputpdf = open("PDF/Calendar2025.pdf", "rb")
+    if os.path.isfile("PDF/Calendar2026.pdf"):
+        inputpdf = open("PDF/Calendar2026.pdf", "rb")
         merger.append(inputpdf)
         inputpdf.close()
-    if os.path.isfile("PDF/Circuits2025LM.pdf"):
-        inputpdf = open("PDF/Circuits2025LM.pdf", "rb")
+    if os.path.isfile("PDF/Circuits2026LM.pdf"):
+        inputpdf = open("PDF/Circuits2026LM.pdf", "rb")
         merger.append(inputpdf)
         inputpdf.close()
-    if os.path.isfile("PDF/Teams2025.pdf"):
-        inputpdf = open("PDF/Teams2025.pdf", "rb")
+    if os.path.isfile("PDF/Teams2026.pdf"):
+        inputpdf = open("PDF/Teams2026.pdf", "rb")
         merger.append(inputpdf)
         inputpdf.close()
-    output = open("PDF/F1Totaal2025.pdf", "wb")
+    output = open("PDF/F1Totaal2026.pdf", "wb")
     merger.write(output)
     merger.close()
     output.close()
