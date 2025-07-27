a7e999af6624f9bcb8ecdf32d04df788c1b8789e
jan438 <jan438@users.noreply.github.com>
2025-07-27 11:23:51 +0200

    print changed

--- Calendar2026.py
+++ Calendar2026.py
@@ -320,7 +320,7 @@ for i in range(len(raceevents)):
             result = result[1][:-1].encode()
             result = result[14:].decode()
             if result == "USA":
-                print(result, len(result))
+                print(result, len(result), raceevent.location)
             my_canvas.setFont("Helvetica", 12)
             my_canvas.drawString(leftmargin + col * colwidth, row * rowheight - 75, result)
             [hour,minute] = converttimetztolocalclock(raceevent.starttime)
