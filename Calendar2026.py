2df4fd004c74a04e0f604d366185302ad1971e9b
jan438 <jan438@users.noreply.github.com>
2025-07-27 14:39:04 +0200

    derde rij ingevuld

--- Calendar2026.py
+++ Calendar2026.py
@@ -244,13 +244,13 @@ my_canvas.drawImage(image, 2 * colwidth + 2.1, 4 * rowheight + bottommargin + 10
 image = "Circuits/Location/Monaco_location_map.png"
 my_canvas.drawImage(image, 3 * colwidth + 2.1, 4 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 
-image = "Circuits/Location/Australia_location_map.png"
+image = "Circuits/Location/Spain_location_map.png"
 my_canvas.drawImage(image, 0 * colwidth + 2.1, 3 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
-image = "Circuits/Location/China_location_map.png"
+image = "Circuits/Location/Austria_location_map.png"
 my_canvas.drawImage(image, 1 * colwidth + 2.1, 3 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
-image = "Circuits/Location/Japan_location_map.png"
+image = "Circuits/Location/United_Kingdom_location_map.png"
 my_canvas.drawImage(image, 2 * colwidth + 2.1, 3 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
-image = "Circuits/Location/Bahrain_location_map.png"
+image = "Circuits/Location/Belgium_location_map.png"
 my_canvas.drawImage(image, 3 * colwidth + 2.1, 3 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 
 image = "Circuits/Location/Australia_location_map.png"
