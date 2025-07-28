ebe27c52b902039e1af078a08f5ad48991f0f6ad
jan438 <jan438@users.noreply.github.com>
2025-07-28 15:39:05 +0200

    clocks added

--- basicclock.py
+++ basicclock.py
@@ -94,7 +94,7 @@ def get_random_times(n, difficulty):
 
     times = []
     for i in range(n):
-        hr = 3
+        hr = 5
         mn = 30
         times.append('{}:{}'.format(hr,mn))
     return times
