fe16a1af9b52d71775f6303546bd823eb7e18b38
jan438 <jan438@users.noreply.github.com>
2025-07-29 11:40:00 +0200

    spain added

--- Calendar2026.py
+++ Calendar2026.py
@@ -255,7 +255,7 @@ my_canvas.circle(3 * colwidth + 2.1 + 35.0, 4 * rowheight + bottommargin + 10 + 
 
 image = "Circuits/Location/Spain_location_map.png"
 my_canvas.drawImage(image, 0 * colwidth + 2.1, 3 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
-renderPDF.draw(scaleSVG("Flags/ES.svg", 0.30), my_canvas, flagoffset_x + 0 * colwidth + 2.1, flagoffset_y + 3 * rowheight + bottommargin + 10)
+renderPDF.draw(scaleSVG("Flags/EStw.svg", 0.5), my_canvas, flagoffset_x + 0 * colwidth + 2.1, flagoffset_y + 3 * rowheight + bottommargin + 10)
 my_canvas.circle(0 * colwidth + 2.1 + 35.0, 3 * rowheight + bottommargin + 10 + 48.0, 4.0, stroke = 0, fill = 1)
 image = "Circuits/Location/Austria_location_map.png"
 my_canvas.drawImage(image, 1 * colwidth + 2.1, 3 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
@@ -284,7 +284,7 @@ renderPDF.draw(scaleSVG("Flags/IT.svg", 0.30), my_canvas, flagoffset_x + 2 * col
 my_canvas.circle(2 * colwidth + 2.1 + 35.0, 2 * rowheight + bottommargin + 10 + 48.0, 4.0, stroke = 0, fill = 1)
 image = "Circuits/Location/Spain_location_map.png"
 my_canvas.drawImage(image, 3 * colwidth + 2.1, 2 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
-renderPDF.draw(scaleSVG("Flags/ES.svg", 0.30), my_canvas, flagoffset_x + 3 * colwidth + 2.1, flagoffset_y + 2 * rowheight + bottommargin + 10)
+renderPDF.draw(scaleSVG("Flags/EStw.svg", 0.5), my_canvas, flagoffset_x + 3 * colwidth + 2.1, flagoffset_y + 2 * rowheight + bottommargin + 10)
 my_canvas.circle(3 * colwidth + 2.1 + 35.0, 2 * rowheight + bottommargin + 10 + 48.0, 4.0, stroke = 0, fill = 1)
 
 image = "Circuits/Location/Azerbaijan_location_map.png"
