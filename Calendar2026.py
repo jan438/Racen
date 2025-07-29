8dec35edc0df9ca4c8fbca638217841ac56dc3e4
jan438 <jan438@users.noreply.github.com>
2025-07-29 11:00:24 +0200

    twee flags added

--- Calendar2026.py
+++ Calendar2026.py
@@ -259,7 +259,7 @@ renderPDF.draw(scaleSVG("Flags/ES.svg", 0.30), my_canvas, flagoffset_x + 0 * col
 my_canvas.circle(0 * colwidth + 2.1 + 35.0, 3 * rowheight + bottommargin + 10 + 48.0, 4.0, stroke = 0, fill = 1)
 image = "Circuits/Location/Austria_location_map.png"
 my_canvas.drawImage(image, 1 * colwidth + 2.1, 3 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
-renderPDF.draw(scaleSVG("Flags/AT.svg", 0.30), my_canvas, flagoffset_x + 1 * colwidth + 2.1, flagoffset_y + 3 * rowheight + bottommargin + 10)
+renderPDF.draw(scaleSVG("Flags/ATtw.svg", 0.5), my_canvas, flagoffset_x + 1 * colwidth + 2.1, flagoffset_y + 3 * rowheight + bottommargin + 10)
 my_canvas.circle(1 * colwidth + 2.1 + 35.0, 3 * rowheight + bottommargin + 10 + 48.0, 4.0, stroke = 0, fill = 1)
 image = "Circuits/Location/United_Kingdom_location_map.png"
 my_canvas.drawImage(image, 2 * colwidth + 2.1, 3 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
@@ -276,7 +276,7 @@ renderPDF.draw(scaleSVG("Flags/HU.svg", 0.30), my_canvas, flagoffset_x + 0 * col
 my_canvas.circle(0 * colwidth + 2.1 + 35.0, 2 * rowheight + bottommargin + 10 + 48.0, 4.0, stroke = 0, fill = 1)
 image = "Circuits/Location/Netherlands_location_map.png"
 my_canvas.drawImage(image, 1 * colwidth + 2.1, 2 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
-renderPDF.draw(scaleSVG("Flags/NL.svg", 0.30), my_canvas, flagoffset_x + 1 * colwidth + 2.1, flagoffset_y + 2 * rowheight + bottommargin + 10)
+renderPDF.draw(scaleSVG("Flags/NLtw.svg", 0.5), my_canvas, flagoffset_x + 1 * colwidth + 2.1, flagoffset_y + 2 * rowheight + bottommargin + 10)
 my_canvas.circle(1 * colwidth + 2.1 + 35.0, 2 * rowheight + bottommargin + 10 + 48.0, 4.0, stroke = 0, fill = 1)
 image = "Circuits/Location/Italy_location_map.png"
 my_canvas.drawImage(image, 2 * colwidth + 2.1, 2 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
