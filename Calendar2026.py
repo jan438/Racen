d25831ce16bcbeb36a6ed0e664f9a70055767feb
jan438 <jan438@users.noreply.github.com>
2025-07-29 11:35:58 +0200

    hungarry flag added

--- Calendar2026.py
+++ Calendar2026.py
@@ -272,7 +272,7 @@ my_canvas.circle(3 * colwidth + 2.1 + 35.0, 3 * rowheight + bottommargin + 10 + 
 
 image = "Circuits/Location/Hungary_location_map.png"
 my_canvas.drawImage(image, 0 * colwidth + 2.1, 2 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
-renderPDF.draw(scaleSVG("Flags/HU.svg", 0.30), my_canvas, flagoffset_x + 0 * colwidth + 2.1, flagoffset_y + 2 * rowheight + bottommargin + 10)
+renderPDF.draw(scaleSVG("Flags/HUtw.svg", 0.5), my_canvas, flagoffset_x + 0 * colwidth + 2.1, flagoffset_y + 2 * rowheight + bottommargin + 10)
 my_canvas.circle(0 * colwidth + 2.1 + 35.0, 2 * rowheight + bottommargin + 10 + 48.0, 4.0, stroke = 0, fill = 1)
 image = "Circuits/Location/Netherlands_location_map.png"
 my_canvas.drawImage(image, 1 * colwidth + 2.1, 2 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
