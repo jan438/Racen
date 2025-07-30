a906b16cb8de2c289d4771c5dbf4052f41d67fa2
jan438 <jan438@users.noreply.github.com>
2025-07-30 10:12:52 +0200

    australia added

--- Calendar2026.py
+++ Calendar2026.py
@@ -222,7 +222,7 @@ my_canvas.setFillColor(HexColor(circuitarea))
 image = "Circuits/Location/Australia_location_map.png"
 my_canvas.drawImage(image, 0 * colwidth + 2.1, 5 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/AUtw.svg", 0.5), my_canvas, flagoffset_x + 0 * colwidth + 2.1, flagoffset_y + 5 * rowheight + bottommargin + 10)
-my_canvas.circle(0 * colwidth + 2.1 + float(circuitsdata[0][25]), 5 * rowheight + bottommargin + 10 + float(circuitsdata[0][26]), 4.0, stroke = 0, fill = 1)
+my_canvas.circle(0 * colwidth + 2.1 + 105, 5 * rowheight + bottommargin + 10 + 20, 4.0, stroke = 0, fill = 1)
 image = "Circuits/Location/China_location_map.png"
 my_canvas.drawImage(image, 1 * colwidth + 2.1, 5 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/CNtw.svg", 0.5), my_canvas, flagoffset_x + 1 * colwidth + 2.1, flagoffset_y + 5 * rowheight + bottommargin + 10)
