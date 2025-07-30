dc23770944d5dcc8b61f3b54d14551f14e68d0f1
jan438 <jan438@users.noreply.github.com>
2025-07-30 16:07:36 +0200

    zover

--- Calendar2026.py
+++ Calendar2026.py
@@ -311,11 +311,11 @@ my_canvas.circle(0 * colwidth + 2.1 + 102, 0 * rowheight + bottommargin + 10 + 4
 image = "Circuits/Location/USA_Nevada_location_map.png"
 my_canvas.drawImage(image, 1 * colwidth + 2.1, 0 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/UStw.svg", 0.5), my_canvas, flagoffset_x + 1 * colwidth + 2.1, flagoffset_y + 0 * rowheight + bottommargin + 10)
-my_canvas.circle(1 * colwidth + 2.1 + 110, 0 * rowheight + bottommargin + 10 + 15, 4.0, stroke = 0, fill = 1)
+my_canvas.circle(1 * colwidth + 2.1 + 115, 0 * rowheight + bottommargin + 10 + 15, 4.0, stroke = 0, fill = 1)
 image = "Circuits/Location/Qatar_location_map.png"
 my_canvas.drawImage(image, 2 * colwidth + 2.1, 0 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/QAtw.svg", 0.5), my_canvas, flagoffset_x + 2 * colwidth + 2.1, flagoffset_y + 0 * rowheight + bottommargin + 10)
-my_canvas.circle(2 * colwidth + 2.1 + 80, 0 * rowheight + bottommargin + 10 + 65, 4.0, stroke = 0, fill = 1)
+my_canvas.circle(2 * colwidth + 2.1 + 75, 0 * rowheight + bottommargin + 10 + 65, 4.0, stroke = 0, fill = 1)
 image = "Circuits/Location/United_Arab_Emirates_location_map.png"
 my_canvas.drawImage(image, 3 * colwidth + 2.1, 0 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/AEtw.svg", 0.5), my_canvas, flagoffset_x + 3 * colwidth + 2.1, flagoffset_y + 0 * rowheight + bottommargin + 10)
