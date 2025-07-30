f6795899d140214f89e513bb6246787a9d2301a8
jan438 <jan438@users.noreply.github.com>
2025-07-30 14:55:31 +0200

    baku added

--- Calendar2026.py
+++ Calendar2026.py
@@ -290,7 +290,7 @@ my_canvas.circle(3 * colwidth + 2.1 + 55, 2 * rowheight + bottommargin + 10 + 55
 image = "Circuits/Location/Azerbaijan_location_map.png"
 my_canvas.drawImage(image, 0 * colwidth + 2.1, 1 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/AZtw.svg", 0.5), my_canvas, flagoffset_x + 0 * colwidth + 2.1, flagoffset_y + 1 * rowheight + bottommargin + 10)
-my_canvas.circle(0 * colwidth + 2.1 + 5, 1 * rowheight + bottommargin + 10 + 5, 4.0, stroke = 0, fill = 1)
+my_canvas.circle(0 * colwidth + 2.1 + 100, 1 * rowheight + bottommargin + 10 + 55, 4.0, stroke = 0, fill = 1)
 image = "Circuits/Location/Singapore_location_map.png"
 my_canvas.drawImage(image, 1 * colwidth + 2.1, 1 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/SGtw.svg", 0.5), my_canvas, flagoffset_x + 1 * colwidth + 2.1, flagoffset_y + 1 * rowheight + bottommargin + 10)
