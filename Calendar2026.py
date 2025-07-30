9ce43f2e2ed8ae0d685de53b060de3d6d33f9573
jan438 <jan438@users.noreply.github.com>
2025-07-30 10:08:24 +0200

    shanhai added

--- Calendar2026.py
+++ Calendar2026.py
@@ -226,7 +226,7 @@ my_canvas.circle(0 * colwidth + 2.1 + float(circuitsdata[0][25]), 5 * rowheight 
 image = "Circuits/Location/China_location_map.png"
 my_canvas.drawImage(image, 1 * colwidth + 2.1, 5 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/CNtw.svg", 0.5), my_canvas, flagoffset_x + 1 * colwidth + 2.1, flagoffset_y + 5 * rowheight + bottommargin + 10)
-my_canvas.circle(1 * colwidth + 2.1 + float(circuitsdata[17][25]), 5 * rowheight + bottommargin + 10 + float(circuitsdata[17][26]), 4.0, stroke = 0, fill = 1)
+my_canvas.circle(1 * colwidth + 2.1 + 105, 5 * rowheight + bottommargin + 10 + 35, 4.0, stroke = 0, fill = 1)
 image = "Circuits/Location/Japan_location_map.png"
 my_canvas.drawImage(image, 2 * colwidth + 2.1, 5 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/JPtw.svg", 0.5), my_canvas, flagoffset_x + 2 * colwidth + 2.1, flagoffset_y + 5 * rowheight + bottommargin + 10)
