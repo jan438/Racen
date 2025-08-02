923e5097c135df31cb64146b285e5bbc832b9f06
jan438 <jan438@users.noreply.github.com>
2025-08-02 14:46:18 +0200

    outside areasetfillcolor

--- Calendar2026.py
+++ Calendar2026.py
@@ -337,11 +337,12 @@ for i in range(len(raceevents)):
     if raceevent is not None:
         subsummary = raceevent.summary[:10]
         if subsummary == "Practice 1":
-            my_canvas.setFillColor(HexColor(text1))
+            my_canvas.setFillColor(HexColor(outsidearea))
             my_canvas.line(col * colwidth + 12.0, row * rowheight + 32.0, col * colwidth + colwidth - 8.0, row * rowheight + 32.0)
             p = my_canvas.beginPath()
             p.arc(col * colwidth + 2.0, row * rowheight + 12.0, col * colwidth + 22.0, row * rowheight + 32.0, startAng = 90, extent = 90)
             my_canvas.drawPath(p, fill = 0, stroke = 1)
+            my_canvas.setFillColor(HexColor(text1))
             my_canvas.line(col * colwidth + 2.0, row * rowheight - 80.0, col * colwidth + 2.0, row * rowheight + 22.0)   
             my_canvas.line(col * colwidth + 2.0, row * rowheight - 80, col * colwidth + 130.0, row * rowheight - 80)
             p = my_canvas.beginPath()
