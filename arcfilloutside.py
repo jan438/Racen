79d39db781f3eaef2268cb9e69f1fb1766c08b60
jan438 <jan438@users.noreply.github.com>
2025-08-02 13:48:54 +0200

    fill outside added

---
+++ arcfilloutside.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import csv
+import math
+import unicodedata
+from pathlib import Path
+from datetime import datetime, date, timedelta
+from reportlab.pdfgen import canvas
+from reportlab.lib.colors import HexColor
+from reportlab.lib.units import inch, mm
+
+if sys.platform[0] == 'l':
+    path = '/home/jan/git/Racen'
+if sys.platform[0] == 'w':
+    path = "C:/Users/janbo/OneDrive/Documents/GitHub/Racen"
+os.chdir(path)
+my_canvas = canvas.Canvas("PDF/arcfilloutside.pdf")
+my_canvas.setFillColor(HexColor('#FECDE5'))
+p = my_canvas.beginPath()
+p.arc(20.0, 20.0, 40.0, 40.0, startAng = 90, extent = 90)
+my_canvas.drawPath(p, fill=1, stroke=1)
+my_canvas.save()
+key = input("Wait")
