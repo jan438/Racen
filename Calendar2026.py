dc2c2b9b70cf1c3634210490e5d67341a058b278
jan438 <jan438@users.noreply.github.com>
2025-08-03 09:23:17 +0200

    removed code

--- Calendar2026.py
+++ Calendar2026.py
@@ -337,7 +337,6 @@ for i in range(len(raceevents)):
     if raceevent is not None:
         subsummary = raceevent.summary[:10]
         if subsummary == "Practice 1":
-            my_canvas.setFillColor(HexColor(outsidearea))
             my_canvas.line(col * colwidth + 12.0, row * rowheight + 32.0, col * colwidth + colwidth - 8.0, row * rowheight + 32.0)
             p = my_canvas.beginPath()
             p.arc(col * colwidth + 2.0, row * rowheight + 12.0, col * colwidth + 22.0, row * rowheight + 32.0, startAng = 90, extent = 90)
