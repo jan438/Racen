456b2b1e291ef368e8196d22b234f4bbb9d06535
jan438 <jan438@users.noreply.github.com>
2025-08-03 13:32:47 +0200

    arc added

--- arcfilloutside.py
+++ arcfilloutside.py
@@ -32,5 +32,10 @@ for i in range(24):
     if col == 4:
         col = 0
         row -= 1
+col = 0
+row = 6
+p = my_canvas.beginPath()
+p.arc(col * colwidth + 120.0, row * rowheight - 80.0, col * colwidth + 140.0, row * rowheight - 60.0, startAng = 270, extent = 90)
+my_canvas.drawPath(p, fill = 0, stroke = 1)
 my_canvas.save()
 key = input("Wait")
