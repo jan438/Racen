064753372053de18e893eb39f0c34b9181733c79
jan438 <jan438@users.noreply.github.com>
2025-08-07 14:34:11 +0200

    yellow added

--- CircuitLM.py
+++ CircuitLM.py
@@ -164,8 +164,8 @@ def GeoJSON_to_SVG(geojsonfile, svgfile):
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
                     path = coordinates_to_path([coords[idx3:idx2]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
-                    #path = coordinates_to_path([coords[idx3 - 1:]], scale, translate)
-                    #f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
+                    path = coordinates_to_path([coords[idx2:]], scale, translate)
+                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
                     #path = coordinates_to_path([coords[:idx1 + 1]], scale, translate)
                     #f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
                 elif cx == 14:
