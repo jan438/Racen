6afe76e4572e54e646db10ae761edd62a9aa836f
jan438 <jan438@users.noreply.github.com>
2025-08-07 08:57:09 +0200

    all sectoren added

--- CircuitLM.py
+++ CircuitLM.py
@@ -153,6 +153,8 @@ def GeoJSON_to_SVG(geojsonfile, svgfile):
                 elif cx == 3:
                     path = coordinates_to_path([coords[idx2:]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
+                    path = coordinates_to_path([coords[:idx3]], scale, translate)
+                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
                     path = coordinates_to_path([coords[idx3:idx1]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
                     path = coordinates_to_path([coords[idx1:idx2]], scale, translate)
