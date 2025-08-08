6c2746ad8c28ee036695c0bd742fdac52e1989e9
jan438 <jan438@users.noreply.github.com>
2025-08-08 14:13:16 +0200

    y coord adapted

--- Circuits2026LM.py
+++ Circuits2026LM.py
@@ -231,7 +231,7 @@ for i in range(count):
     worldlocy = worldmap_y + float(circuitsdata[i][4])
     my_canvas.setFillColor(HexColor(circuitcolors[i]))
     my_canvas.circle(worldlocx, worldlocy, 1.5, stroke = 0, fill = 1)
-    my_canvas.circle(circuit_x + left_margin + int(circuitsdata[i][10]) + 25, circuit_y + bottom_margin + int(circuitsdata[i][11]) + 15, 1.5, stroke = 0, fill = 1)
+    my_canvas.circle(circuit_x + left_margin + int(circuitsdata[i][10]) + 25, circuit_y + bottom_margin + int(circuitsdata[i][11]) + 14, 1.5, stroke = 0, fill = 1)
     col += 1
     if col == colcount:
         row = row - 1
