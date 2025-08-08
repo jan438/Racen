db6c2baac465557c2625e0ed20225beefbb93638
jan438 <jan438@users.noreply.github.com>
2025-08-08 10:01:43 +0200

    circuit colors added

--- Circuits2026LM.py
+++ Circuits2026LM.py
@@ -11,6 +11,7 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import inch, mm
+from reportlab.lib.colors import HexColor
 from reportlab.graphics.shapes import *
 from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
 
@@ -227,7 +228,8 @@ for i in range(count):
     my_canvas.drawString(circuit_x + left_margin + 20 + int(circuitsdata[i][10]), circuit_y + bottom_margin + int(circuitsdata[i][11]) + 3, f"{altitude}")
     worldlocx = worldmap_x + float(circuitsdata[i][3])
     worldlocy = worldmap_y + float(circuitsdata[i][4])
-    my_canvas.setFillColorRGB(255,0,0)
+    circuitcolor = "#FF0000"
+    my_canvas.setFillColor(HexColor(circuitcolor))
     my_canvas.circle(worldlocx, worldlocy, 1.5, stroke = 0, fill = 1)
     col += 1
     if col == colcount:
