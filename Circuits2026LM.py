2af8e87e9f2c5482b9d1b9b81a8e9f9ba6f685db
jan438 <jan438@users.noreply.github.com>
2025-08-08 14:04:04 +0200

    a ctweede rrcel added

--- Circuits2026LM.py
+++ Circuits2026LM.py
@@ -231,6 +231,7 @@ for i in range(count):
     worldlocy = worldmap_y + float(circuitsdata[i][4])
     my_canvas.setFillColor(HexColor(circuitcolors[i]))
     my_canvas.circle(worldlocx, worldlocy, 1.5, stroke = 0, fill = 1)
+    my_canvas.circle(  circuit_x + left_margin + int(circuitsdata[i][10]) + 30 ,  circuit_y + bottom_margin + int(circuitsdata[i][11])  , 1.5, stroke = 0, fill = 1)
     col += 1
     if col == colcount:
         row = row - 1
