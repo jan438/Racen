4abcd9882c6606aa5ee79bad069e08b7add02f34
jan438 <jan438@users.noreply.github.com>
2025-08-09 09:18:25 +0200

    legenda circle worldcolor

--- Circuits2026LM.py
+++ Circuits2026LM.py
@@ -34,6 +34,7 @@ altitudescale = 0.010
 worldmapscale = 0.34
 worldmap_x = 125
 worldmap_y = 315
+worldcolor ="#9BD5C1"
 circuitcolors = ["#8250C4", "#ECC846", "#73B761", "#118DFF", "#A3623A", "#FF0000", "#FF0000", "#00FF00", "#FF0000", "#FF0000", "#FF0000", "#00FF00", "#FF0000", "#FF0000", "#FF0000", "#00FF00", "#FF0000", "#FF0000", "#FF0000", "#00FF00", "#FF0000", "#FF0000", "#FF0000", "#00FF00"]
 
 def scaleSVG(svgfile, scaling_factor):
@@ -231,6 +232,9 @@ for i in range(count):
     worldlocy = worldmap_y + float(circuitsdata[i][4])
     my_canvas.setFillColor(HexColor(circuitcolors[i]))
     my_canvas.circle(worldlocx, worldlocy, 1.5, stroke = 0, fill = 1)
+    my_canvas.setFillColor(HexColor(worldcolor))
+    my_canvas.circle(circuit_x + left_margin + int(circuitsdata[i][10]) + 25, circuit_y + bottom_margin + int(circuitsdata[i][11]) + 14, 2.5, stroke = 0, fill = 1)
+    my_canvas.setFillColor(HexColor(circuitcolors[i]))
     my_canvas.circle(circuit_x + left_margin + int(circuitsdata[i][10]) + 25, circuit_y + bottom_margin + int(circuitsdata[i][11]) + 14, 1.5, stroke = 0, fill = 1)
     col += 1
     if col == colcount:
