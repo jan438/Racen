ac11139ae3e4534e55c93ca9d79a51458aafbe69
jan438 <jan438@users.noreply.github.com>
2025-08-15 09:48:38 +0200

    hungaring added

--- CircuitLC.py
+++ CircuitLC.py
@@ -187,14 +187,8 @@ def GeoJSON_to_SVG(geojsonfile, svgfile):
                     path = coordinates_to_path([coords[:idx1 + 1]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
                 elif cx == 6:
-                    path = coordinates_to_path([coords[:idx3 + 1]], scale, translate)
+                    path = coordinates_to_path([coords], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
-                    path = coordinates_to_path([coords[idx1:]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
-                    path = coordinates_to_path([coords[idx2:idx1 + 1]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
-                    path = coordinates_to_path([coords[idx3:idx2 + 1]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
                 elif cx == 7:
                     path = coordinates_to_path([coords[idx2:idx3]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
