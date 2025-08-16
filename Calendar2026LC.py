3f74cd0e79b60d38fac11ed4753aa93781efcca0
jan438 <jan438@users.noreply.github.com>
2025-08-16 13:43:44 +0200

    miami added

--- Calendar2026LC.py
+++ Calendar2026LC.py
@@ -259,6 +259,9 @@ image = "Circuits/Location/USA_Florida_location_map.png"
 my_canvas.drawImage(image, 1 * colwidth + leftmargin, 4 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/UStw.svg", 0.5), my_canvas, flagoffset_x + 1 * colwidth + leftmargin, flagoffset_y + 4 * rowheight + bottommargin + 10)
 my_canvas.circle(1 * colwidth + leftmargin + 130, 4 * rowheight + bottommargin + 10 + 25, 4.0, stroke = 0, fill = 1)
+circuit_x = 60
+circuit_y = 30
+renderPDF.draw(scaleSVG("SVG/MiamiLC.svg", circuitscale), my_canvas, circuit_x + 1 * colwidth + leftmargin, circuit_y + 4 * rowheight + bottommargin + 10)
 image = "Circuits/Location/Canada_location_map.png"
 my_canvas.drawImage(image, 2 * colwidth + leftmargin, 4 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/CAtw.svg", 0.5), my_canvas, flagoffset_x + 2 * colwidth + leftmargin, flagoffset_y + 4 * rowheight + bottommargin + 10)
