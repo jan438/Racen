b1e70d791a9e07751037dec038003854edaccdf8
jan438 <jan438@users.noreply.github.com>
2025-08-16 08:58:45 +0200

    hermanas added

--- Calendar2026LC.py
+++ Calendar2026LC.py
@@ -280,6 +280,8 @@ image = "Circuits/Location/Hungary_location_map.png"
 my_canvas.drawImage(image, 0 * colwidth + leftmargin, 2 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/HUtw.svg", 0.5), my_canvas, flagoffset_x + 0 * colwidth + leftmargin, flagoffset_y + 2 * rowheight + bottommargin + 10)
 my_canvas.circle(0 * colwidth + leftmargin + 58, 2 * rowheight + bottommargin + 10 + 60, 4.0, stroke = 0, fill = 1)
+circuit_x = 30
+circuit_y = 30
 renderPDF.draw(scaleSVG("SVG/HungaroringLC.svg", circuitscale), my_canvas, circuit_x + 0 * colwidth + leftmargin, circuit_y + 2 * rowheight + bottommargin + 10)
 image = "Circuits/Location/Netherlands_location_map.png"
 my_canvas.drawImage(image, 1 * colwidth + leftmargin, 2 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
@@ -310,6 +312,8 @@ image = "Circuits/Location/Mexico_location_map.png"
 my_canvas.drawImage(image, 3 * colwidth + leftmargin, 1 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/MXtw.svg", 0.5), my_canvas, flagoffset_x + 3 * colwidth + leftmargin, flagoffset_y + 1 * rowheight + bottommargin + 10)
 my_canvas.circle(3 * colwidth + leftmargin + 75, 1 * rowheight + bottommargin + 10 + 55, 4.0, stroke = 0, fill = 1)
+circuit_x = 55
+circuit_y = 15
 renderPDF.draw(scaleSVG("SVG/HermanosRodríguezLC.svg", circuitscale), my_canvas, circuit_x + 3 * colwidth + leftmargin, circuit_y + 1 * rowheight + bottommargin + 10)
 
 image = "Circuits/Location/Brazil_location_map.png"
