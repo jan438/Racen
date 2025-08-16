4665cd7c2f8ba4233cbc6228c07d7ee1746392c6
jan438 <jan438@users.noreply.github.com>
2025-08-16 13:32:18 +0200

    singapore added

--- Calendar2026LC.py
+++ Calendar2026LC.py
@@ -325,6 +325,9 @@ image = "Circuits/Location/Singapore_location_map.png"
 my_canvas.drawImage(image, 1 * colwidth + leftmargin, 1 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/SGtw.svg", 0.5), my_canvas, flagoffset_x + 1 * colwidth + leftmargin, flagoffset_y + 1 * rowheight + bottommargin + 10)
 my_canvas.circle(1 * colwidth + leftmargin + 75, 1 * rowheight + bottommargin + 10 + 45, 4.0, stroke = 0, fill = 1)
+circuit_x = 55
+circuit_y = 15
+renderPDF.draw(scaleSVG("SVG/MarinaBayLC.svg", circuitscale), my_canvas, circuit_x + 1 * colwidth + leftmargin, circuit_y + 1 * rowheight + bottommargin + 10)
 image = "Circuits/Location/USA_Texas_location_map.png"
 my_canvas.drawImage(image, 2 * colwidth + leftmargin, 1 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/UStw.svg", 0.5), my_canvas, flagoffset_x + 2 * colwidth + leftmargin, flagoffset_y + 1 * rowheight + bottommargin + 10)
