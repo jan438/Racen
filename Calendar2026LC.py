a2f1d0ccfed10721d392adc9d31ea4fdfb27c436
jan438 <jan438@users.noreply.github.com>
2025-08-16 13:50:54 +0200

    monaco added

--- Calendar2026LC.py
+++ Calendar2026LC.py
@@ -273,6 +273,9 @@ image = "Circuits/Location/Monaco_location_map.png"
 my_canvas.drawImage(image, 3 * colwidth + leftmargin, 4 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/MCtw.svg", 0.5), my_canvas, flagoffset_x + 3 * colwidth + leftmargin, flagoffset_y + 4 * rowheight + bottommargin + 10)
 my_canvas.circle(3 * colwidth + leftmargin + 60, 4 * rowheight + bottommargin + 10 + 55, 4.0, stroke = 0, fill = 1)
+circuit_x = 60
+circuit_y = 30
+renderPDF.draw(scaleSVG("SVG/MonacoLC.svg", circuitscale), my_canvas, circuit_x + 3 * colwidth + leftmargin, circuit_y + 4 * rowheight + bottommargin + 10)
 
 image = "Circuits/Location/Spain_location_map.png"
 my_canvas.drawImage(image, 0 * colwidth + leftmargin, 3 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
