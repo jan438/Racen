281563e28965350a8f08c93e1d9e223ad81bc138
jan438 <jan438@users.noreply.github.com>
2025-08-16 08:42:06 +0200

    circuit scale

--- Calendar2026LC.py
+++ Calendar2026LC.py
@@ -219,6 +219,7 @@ rowheight = 120
 my_canvas.setFont(calfont, 12)
 bottommargin = 30
 leftmargin = 8.0
+circuitscale = 0.06
 row = 5
 col = 0
 eventwidth = 137
@@ -279,7 +280,7 @@ image = "Circuits/Location/Hungary_location_map.png"
 my_canvas.drawImage(image, 0 * colwidth + leftmargin, 2 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/HUtw.svg", 0.5), my_canvas, flagoffset_x + 0 * colwidth + leftmargin, flagoffset_y + 2 * rowheight + bottommargin + 10)
 my_canvas.circle(0 * colwidth + leftmargin + 58, 2 * rowheight + bottommargin + 10 + 60, 4.0, stroke = 0, fill = 1)
-renderPDF.draw(scaleSVG("SVG/HungaroringLC.svg", 0.08), my_canvas, circuit_x + 0 * colwidth + leftmargin, circuit_y + 2 * rowheight + bottommargin + 10)
+renderPDF.draw(scaleSVG("SVG/HungaroringLC.svg", circuitscale), my_canvas, circuit_x + 0 * colwidth + leftmargin, circuit_y + 2 * rowheight + bottommargin + 10)
 image = "Circuits/Location/Netherlands_location_map.png"
 my_canvas.drawImage(image, 1 * colwidth + leftmargin, 2 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/NLtw.svg", 0.5), my_canvas, flagoffset_x + 1 * colwidth + leftmargin, flagoffset_y + 2 * rowheight + bottommargin + 10)
@@ -309,7 +310,7 @@ image = "Circuits/Location/Mexico_location_map.png"
 my_canvas.drawImage(image, 3 * colwidth + leftmargin, 1 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/MXtw.svg", 0.5), my_canvas, flagoffset_x + 3 * colwidth + leftmargin, flagoffset_y + 1 * rowheight + bottommargin + 10)
 my_canvas.circle(3 * colwidth + leftmargin + 75, 1 * rowheight + bottommargin + 10 + 55, 4.0, stroke = 0, fill = 1)
-renderPDF.draw(scaleSVG("SVG/HermanosRodríguezLC.svg", 0.08), my_canvas, circuit_x + 3 * colwidth + leftmargin, circuit_y + 1 * rowheight + bottommargin + 10)
+renderPDF.draw(scaleSVG("SVG/HermanosRodríguezLC.svg", circuitscale), my_canvas, circuit_x + 3 * colwidth + leftmargin, circuit_y + 1 * rowheight + bottommargin + 10)
 
 image = "Circuits/Location/Brazil_location_map.png"
 my_canvas.drawImage(image, 0 * colwidth + leftmargin, 0 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
