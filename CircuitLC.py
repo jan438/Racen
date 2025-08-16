d944948e113e6d20750678ef967487f70dbddd81
jan438 <jan438@users.noreply.github.com>
2025-08-16 14:14:43 +0200

    silverstone added

--- CircuitLC.py
+++ CircuitLC.py
@@ -11,7 +11,7 @@ from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
 
 circuitscale = 1.0
 flagcorrection = -5.0
-cx = 17
+cx = 18
 sec1color = "#db4a25"   #red
 sec2color = "#58fdff"   #blue
 sec3color = "#fae44a"   #yellow
@@ -198,14 +198,9 @@ def GeoJSON_to_SVG(geojsonfile, svgfile):
                     path = coordinates_to_path([coords], scale, translate)
                     f.write(f'<path d="{path}" fill="{cc}" stroke="none"/>\n')
                 elif cx == 18:
-                    path = coordinates_to_path([coords[idx3 - 1:idx1 + 1]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
-                    path = coordinates_to_path([coords[:idx3]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
-                    path = coordinates_to_path([coords[idx2:]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
-                    path = coordinates_to_path([coords[idx1:idx2 + 1]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
+                    cc = circuitcolors[18]
+                    path = coordinates_to_path([coords], scale, translate)
+                    f.write(f'<path d="{path}" fill="{cc}" stroke="none"/>\n')
                 elif cx == 19:
                     path = coordinates_to_path([coords[:idx2 + 1]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
