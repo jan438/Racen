9c68baa69a71fa1feca4b992658a5a571826dd25
jan438 <jan438@users.noreply.github.com>
2025-08-16 14:18:44 +0200

    siverstone added

--- Calendar2026LC.py
+++ Calendar2026LC.py
@@ -298,6 +298,9 @@ image = "Circuits/Location/United_Kingdom_location_map.png"
 my_canvas.drawImage(image, 2 * colwidth + leftmargin, 3 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/GBtw.svg", 0.5), my_canvas, flagoffset_x + 2 * colwidth + leftmargin, flagoffset_y + 3 * rowheight + bottommargin + 10)
 my_canvas.circle(2 * colwidth + leftmargin + 105, 3 * rowheight + bottommargin + 40 + 5, 4.0, stroke = 0, fill = 1)
+circuit_x = 60
+circuit_y = 30
+renderPDF.draw(scaleSVG("SVG/SilverstoneLC.svg", circuitscale), my_canvas, circuit_x + 2 * colwidth + leftmargin, circuit_y + 3 * rowheight + bottommargin + 10)
 image = "Circuits/Location/Belgium_location_map.png"
 my_canvas.drawImage(image, 3 * colwidth + leftmargin, 3 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/BEtw.svg", 0.5), my_canvas, flagoffset_x + 3 * colwidth + leftmargin, flagoffset_y + 3 * rowheight + bottommargin + 10)
