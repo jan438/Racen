04714f9fb9a37a594f5a654a46bcbc02d7931977
jan438 <jan438@users.noreply.github.com>
2025-08-16 10:45:19 +0200

    brazil added

--- Calendar2026LC.py
+++ Calendar2026LC.py
@@ -338,6 +338,9 @@ image = "Circuits/Location/Brazil_location_map.png"
 my_canvas.drawImage(image, 0 * colwidth + leftmargin, 0 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/BRtw.svg", 0.5), my_canvas, flagoffset_x + 0 * colwidth + leftmargin, flagoffset_y + 0 * rowheight + bottommargin + 10)
 my_canvas.circle(0 * colwidth + leftmargin + 92, 0 * rowheight + bottommargin + 10 + 33, 4.0, stroke = 0, fill = 1)
+circuit_x = 55
+circuit_y = 15
+renderPDF.draw(scaleSVG("SVG/JoseCarlosPaceLC.svg", circuitscale), my_canvas, circuit_x + 0 * colwidth + leftmargin, circuit_y + 0 * rowheight + bottommargin + 10)
 image = "Circuits/Location/USA_Nevada_location_map.png"
 my_canvas.drawImage(image, 1 * colwidth + leftmargin, 0 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/UStw.svg", 0.5), my_canvas, flagoffset_x + 1 * colwidth + leftmargin, flagoffset_y + 0 * rowheight + bottommargin + 10)
