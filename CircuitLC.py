971b8af3fb77d241b14c58b29577b2ee4f9b2f88
jan438 <jan438@users.noreply.github.com>
2025-08-16 14:50:28 +0200

    zandvoort added

--- CircuitLC.py
+++ CircuitLC.py
@@ -11,7 +11,7 @@ from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
 
 circuitscale = 1.0
 flagcorrection = -5.0
-cx = 22
+cx = 23
 sec1color = "#db4a25"   #red
 sec2color = "#58fdff"   #blue
 sec3color = "#fae44a"   #yellow
@@ -218,19 +218,9 @@ def GeoJSON_to_SVG(geojsonfile, svgfile):
                     path = coordinates_to_path([coords], scale, translate)
                     f.write(f'<path d="{path}" fill="{cc}" stroke="none"/>\n')
                 elif cx == 23:
-                    path = coordinates_to_path([coords[idx1:idx2 + 1]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
-                    path = coordinates_to_path([coords[idx2 - 1:idx3 + 1]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
-                    path = coordinates_to_path([coords[idx3 - 1:]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
-                else:
-                    path = coordinates_to_path([coords[idx1:idx2 + 1]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
-                    path = coordinates_to_path([coords[idx2 - 1:idx3 + 1]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
-                    path = coordinates_to_path([coords[idx3 - 1:]], scale, translate)
-                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
+                    cc = circuitcolors[23]
+                    path = coordinates_to_path([coords], scale, translate)
+                    f.write(f'<path d="{path}" fill="{cc}" stroke="none"/>\n')
         f.write('</svg>')    
     return [offset_x, offset_y]
 def transform_svg(svgfile, tx, ty, sx, sy): 
