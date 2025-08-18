082e9900223c3ab4e04f887122677285004814c3
jan438 <jan438@users.noreply.github.com>
2025-08-18 09:55:28 +0200

    practice 3 added

--- Calendar2026LC.py
+++ Calendar2026LC.py
@@ -492,6 +492,11 @@ for i in range(len(raceevents)):
             if result[:10] == "Practice 3":
                 renderPDF.draw(scaleSVG("SVG/stopwatchom.svg", 0.21), my_canvas, leftmargin + col * colwidth + 5.9, row * rowheight - 36.9)
                 my_canvas.drawString(leftmargin + col * colwidth + 11.0, row * rowheight - 33.5, "3")
+                [hour,minute] = converttimetztolocalclock(raceevent.starttime)
+                strhour = "{:02d}".format(hour)
+                strminute = "{:02d}".format(minute)
+                startevent = strhour + strminute
+                renderPDF.draw(scaleSVG("Clocks/" + startevent + "om.svg", 0.21), my_canvas, leftmargin + col * colwidth + 18.0, row * rowheight - 36.1) 
             else:
                 my_canvas.drawString(leftmargin + col * colwidth + 5.9, row * rowheight - 34, result)
                 [hour,minute] = converttimetztolocalclock(raceevent.starttime)
