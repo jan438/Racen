36bf58662733f7b81ce6e5c33d093edb97e1a4a2
jan438 <jan438@users.noreply.github.com>
2025-08-22 14:14:42 +0200

    monxa added

--- Calendar2026LC.py
+++ Calendar2026LC.py
@@ -443,6 +443,7 @@ for i in range(len(raceevents)):
             eventday2_x = 10
             eventday2_y = 0
             if raceevent.description == "Monza":
+                eventday1_x = float(circuitsdata[cx][30])
                 print(cx, raceevent.description, circuitsdata[cx][0], circuitsdata[cx][30])
             my_canvas.line(leftmargin + col * colwidth + 9.9, row * rowheight + 32.0, leftmargin + col * colwidth + colwidth - 10.1, row * rowheight + 32.0)
             p = my_canvas.beginPath()
