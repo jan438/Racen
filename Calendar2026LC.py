f90f5e350cd68e526dd8c19efb20695a8558699c
jan438 <jan438@users.noreply.github.com>
2025-08-25 10:57:32 +0200

    marnabay added

--- Calendar2026LC.py
+++ Calendar2026LC.py
@@ -350,16 +350,13 @@ image = "Circuits/Location/Singapore_location_map.png"
 my_canvas.drawImage(image, 1 * colwidth + leftmargin, 1 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/SGtw.svg", 0.5), my_canvas, flagoffset_x + 1 * colwidth + leftmargin, flagoffset_y + 1 * rowheight + bottommargin + 10)
 my_canvas.circle(1 * colwidth + leftmargin + 75, 1 * rowheight + bottommargin + 10 + 45, 4.0, stroke = 0, fill = 1)
-circuit_x = 55
-circuit_y = 15
-renderPDF.draw(scaleSVG("SVG/MarinaBayLC.svg", circuitscale), my_canvas, circuit_x + 1 * colwidth + leftmargin, circuit_y + 1 * rowheight + bottommargin + 10)
+#circuit_x = 55
+#circuit_y = 15
+#renderPDF.draw(scaleSVG("SVG/MarinaBayLC.svg", circuitscale), my_canvas, circuit_x + 1 * colwidth + leftmargin, circuit_y + 1 * rowheight + bottommargin + 10)
 image = "Circuits/Location/USA_Texas_location_map.png"
 my_canvas.drawImage(image, 2 * colwidth + leftmargin, 1 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/UStw.svg", 0.5), my_canvas, flagoffset_x + 2 * colwidth + leftmargin, flagoffset_y + 1 * rowheight + bottommargin + 10)
 my_canvas.circle(2 * colwidth + leftmargin + 85, 1 * rowheight + bottommargin + 10 + 42, 4.0, stroke = 0, fill = 1)
-#circuit_x = 55
-#circuit_y = 15
-#renderPDF.draw(scaleSVG("SVG/theAmericasLC.svg", circuitscale), my_canvas, circuit_x + 2 * colwidth + leftmargin, circuit_y + 1 * rowheight + bottommargin + 10)
 image = "Circuits/Location/Mexico_location_map.png"
 my_canvas.drawImage(image, 3 * colwidth + leftmargin, 1 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/MXtw.svg", 0.5), my_canvas, flagoffset_x + 3 * colwidth + leftmargin, flagoffset_y + 1 * rowheight + bottommargin + 10)
