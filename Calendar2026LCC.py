a6f724774c0e321f893e86d29132bd7e5ee482c3
jan438 <jan438@users.noreply.github.com>
2025-08-27 10:32:13 +0200

    china added

--- Calendar2026LCC.py
+++ Calendar2026LCC.py
@@ -229,7 +229,6 @@ renderPDF.draw(scaleSVG("Flags/AUtw.svg", 0.5), my_canvas, flagoffset_x + 0 * co
 image = "Circuits/Location/China_location_map.png"
 my_canvas.drawImage(image, 1 * colwidth + leftmargin, 5 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/CNtw.svg", 0.5), my_canvas, flagoffset_x + 1 * colwidth + leftmargin, flagoffset_y + 5 * rowheight + bottommargin + 10)
-my_canvas.circle(1 * colwidth + leftmargin + 90, 5 * rowheight + bottommargin + 20 + 35, 4.0, stroke = 0, fill = 1)
 image = "Circuits/Location/Japan_location_map.png"
 my_canvas.drawImage(image, 2 * colwidth + leftmargin, 5 * rowheight + bottommargin + 10, width=eventwidth, height=eventheight, mask=None)
 renderPDF.draw(scaleSVG("Flags/JPtw.svg", 0.5), my_canvas, flagoffset_x + 2 * colwidth + leftmargin, flagoffset_y + 5 * rowheight + bottommargin + 10)
