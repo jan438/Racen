0ac2fc40f540b785226b7982308d8152c594d609
jan438 <jan438@users.noreply.github.com>
2025-09-01 14:16:35 +0200

    gele sector added

--- CircuitLM.py
+++ CircuitLM.py
@@ -197,8 +197,8 @@ def GeoJSON_to_SVG(cx, geojsonfile, svgfile):
                 elif cx == 7: #JeddahCorniche
                     path = coordinates_to_path([coords[idx2:idx1]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
-                    #path = coordinates_to_path([coords[:idx2 + 1]], scale, translate)
-                    #f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
+                    path = coordinates_to_path([coords[idx3:]], scale, translate)
+                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
                     #path = coordinates_to_path([coords[idx3 - 1:]], scale, translate)
                     #f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
                 elif cx == 8:
