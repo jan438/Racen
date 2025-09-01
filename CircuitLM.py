0e83160988ec5b513fe66e114d5ae306166cd818
jan438 <jan438@users.noreply.github.com>
2025-09-01 11:27:38 +0200

    removed code

--- CircuitLM.py
+++ CircuitLM.py
@@ -11,7 +11,6 @@ from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
 
 circuitscale = 1.0
 flagcorrection = -5.0
-cx = 6
 sec1color = "#db4a25"   #red
 sec2color = "#58fdff"   #blue
 sec3color = "#fae44a"   #yellow
