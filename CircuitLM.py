24782e14f26dfe0da33c799a4efebefd7c2444f4
jan438 <jan438@users.noreply.github.com>
2025-09-01 13:38:11 +0200

    comment added

--- CircuitLM.py
+++ CircuitLM.py
@@ -194,7 +194,7 @@ def GeoJSON_to_SVG(cx, geojsonfile, svgfile):
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
                     path = coordinates_to_path([coords[idx3:idx2 + 1]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
-                elif cx == 7:
+                elif cx == 7: #JeddahCorniche
                     path = coordinates_to_path([coords[idx2:idx3]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
                     path = coordinates_to_path([coords[:idx2 + 1]], scale, translate)
