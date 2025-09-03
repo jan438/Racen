33a3f104e2752366f42a6787d390ac9ee8db0d23
jan438 <jan438@users.noreply.github.com>
2025-09-03 08:55:12 +0200

    the amercas in de beurt

--- CircuitLM.py
+++ CircuitLM.py
@@ -366,7 +366,7 @@ with open(file_to_open, 'r') as file:
         circuitsdata.append(row)
         count += 1
 for i in range(len(circuitsdata)):
-    if circuitsdata[i][0] == "Lusail":
+    if circuitsdata[i][0] == "theAmericas":
         cx = i
         my_canvas = canvas.Canvas("PDF/" + circuitsdata[cx][0] + "2025LM.pdf")
         my_canvas.setFont("Helvetica", 25)
