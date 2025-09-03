d8195ab6a88d6c041ff2dc2b80a1859f95bfac1e
jan438 <jan438@users.noreply.github.com>
2025-09-03 09:32:17 +0200

    gelse aector added

--- CircuitLM.py
+++ CircuitLM.py
@@ -317,8 +317,8 @@ def GeoJSON_to_SVG(cx, geojsonfile, svgfile):
                 elif cx == 21:  #theAmericas
                     path = coordinates_to_path([coords[idx2:idx1]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
-                    #path = coordinates_to_path([coords[idx3 - 1:idx2 + 1]], scale, translate)
-                    #f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
+                    path = coordinates_to_path([coords[idx1:idx3]], scale, translate)
+                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
                     #path = coordinates_to_path([coords[:idx3]], scale, translate)
                     #f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
                 elif cx == 22:
