109204331283f62b0f10503ddd35c2697d7d2025
jan438 <jan438@users.noreply.github.com>
2025-09-03 09:41:32 +0200

    allsectoreb added

--- CircuitLM.py
+++ CircuitLM.py
@@ -321,6 +321,8 @@ def GeoJSON_to_SVG(cx, geojsonfile, svgfile):
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
                     path = coordinates_to_path([coords[:idx2]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
+                    path = coordinates_to_path([coords[idx3:]], scale, translate)
+                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
                 elif cx == 22:
                     path = coordinates_to_path([coords[:idx2 + 1]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
