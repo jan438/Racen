818e6312ca6278e7a387b215a7299c9a95524e14
jan438 <jan438@users.noreply.github.com>
2025-09-06 15:10:59 +0200

    titel changed

--- Teams2026.py
+++ Teams2026.py
@@ -90,7 +90,7 @@ leftmargin = 1.5
 rightmargin = 1.5
 #3 * colwidth + leftmarin + rightmargin = 3 +  3 * 198
 maxscorewidth = 27.0
-d.add(String(100, 775,"Teams", fontSize = 30, fillColor = colors.black))
+d.add(String(100, 775,"2026 Teams", fontSize = 30, fillColor = colors.black))
 for i in range(count):
     #team 0
     d.add(String(leftmargin + col * colwidth, row * rowheight + 135, formule1data[i][0], font = formule1font, fontSize = 22, fillColor = colors.black))
