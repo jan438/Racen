598eda068583f8a2823695783fb3e8c44c169a20
jan438 <jan438@users.noreply.github.com>
2025-09-09 13:47:16 +0200

    bottommargin added

--- Teams2026.py
+++ Teams2026.py
@@ -90,53 +90,54 @@ flagy = 115
 birthdayx = 0
 birthdayy = 115
 leftmargin = 4.0
+bottommargin = 0.0
 d.add(String(100, 775,"2026 Teams", fontSize = 30, fillColor = colors.black))
 for i in range(count):
     #team name
-    d.add(String(leftmargin + col * colwidth, row * rowheight + 135, formule1data[i][0], font = formule1font, fontSize = 20, fillColor = colors.black))
+    d.add(String(leftmargin + col * colwidth, bottommargin + row * rowheight + 135, formule1data[i][0], font = formule1font, fontSize = 20, fillColor = colors.black))
     #name2 7
-    d.add(String(leftmargin + col * colwidth, row * rowheight + namey, formule1data[i][7], font = formule1font, fontSize = 15, fillColor = colors.black))
+    d.add(String(leftmargin + col * colwidth, bottommargin + row * rowheight + namey, formule1data[i][7], font = formule1font, fontSize = 15, fillColor = colors.black))
     #surname2 6
-    d.add(String(leftmargin + col * colwidth, row * rowheight + surname, formule1data[i][6], font = formule1font, fontSize = 14, fillColor = colors.black))
+    d.add(String(leftmargin + col * colwidth, bottommargin + row * rowheight + surname, formule1data[i][6], font = formule1font, fontSize = 14, fillColor = colors.black))
     #name1 3
-    d.add(String(leftmargin + col * colwidth + 0.5 * colwidth, row * rowheight + namey, formule1data[i][3], font = formule1font, fontSize = 15, fillColor = colors.black))
+    d.add(String(leftmargin + col * colwidth + 0.5 * colwidth, bottommargin + row * rowheight + namey, formule1data[i][3], font = formule1font, fontSize = 15, fillColor = colors.black))
     #surname1 2
-    d.add(String(leftmargin + col * colwidth + 0.5 * colwidth, row * rowheight + surname, formule1data[i][2], font = formule1font, fontSize = 14, fillColor = colors.black))
+    d.add(String(leftmargin + col * colwidth + 0.5 * colwidth, bottommargin + row * rowheight + surname, formule1data[i][2], font = formule1font, fontSize = 14, fillColor = colors.black))
     # logo
     svg_root = load_svg_file("Logos/" + formule1data[i][1] + ".svg")
     svgwidth = svg_root.attrib.get('width')
     svgheight = svg_root.attrib.get('height')
     print(formule1data[i][1] + ".svg", "SVGWidth", svgwidth, "SVGHeight", svgheight, "Logowidth", float(formule1data[i][10]), "Logoheight", float(formule1data[i][11]), "Scalingx", float(formule1data[i][12]), "Scalingy", float(formule1data[i][13]))
-    d.add(transform_svg("Logos/" + formule1data[i][1] + ".svg", logox - 0.5 * float(formule1data[i][10]) + col * colwidth, logoy -  0.5 * float(formule1data[i][11]) + row * rowheight, float(formule1data[i][12]), float(formule1data[i][13])))
+    d.add(transform_svg("Logos/" + formule1data[i][1] + ".svg", logox - 0.5 * float(formule1data[i][10]) + col * colwidth, logoy -  0.5 * float(formule1data[i][11]) + bottommargin + row * rowheight, float(formule1data[i][12]), float(formule1data[i][13])))
     #
     # birthday1
     birthday = formule1data[i][8]
-    d.add(transform_svg("SVG/calendar-blank.svg", leftmargin + col * colwidth + birthdayx, row * rowheight + birthdayy, 0.5 , 0.5))
-    d.add(String(leftmargin + col * colwidth + birthdayx, row * rowheight + birthdayy + 6, birthday[:5], fontSize = 8, fillColor = colors.black))
-    d.add(String(leftmargin + col * colwidth + birthdayx, row * rowheight + birthdayy, birthday[6:10], fontSize = 8, fillColor = colors.black))
+    d.add(transform_svg("SVG/calendar-blank.svg", leftmargin + col * colwidth + birthdayx, bottommargin + row * rowheight + birthdayy, 0.5 , 0.5))
+    d.add(String(leftmargin + col * colwidth + birthdayx, bottommargin + row * rowheight + birthdayy + 6, birthday[:5], fontSize = 8, fillColor = colors.black))
+    d.add(String(leftmargin + col * colwidth + birthdayx, bottommargin + row * rowheight + birthdayy, birthday[6:10], fontSize = 8, fillColor = colors.black))
     #land1 5
     landcode = formule1data[i][5]
-    d.add(transform_svg(lookupflag(landcode), col * colwidth + 0.5 * colwidth + flagx, row * rowheight + flagy, 0.3 , 0.3))
+    d.add(transform_svg(lookupflag(landcode), col * colwidth + 0.5 * colwidth + flagx, bottommargin + row * rowheight + flagy, 0.3 , 0.3))
     #birthday2
     birthday = formule1data[i][4]
-    d.add(transform_svg("SVG/calendar-blank.svg", leftmargin + col * colwidth + 0.5 * colwidth + birthdayx, row * rowheight + birthdayy, 0.5 , 0.5))
-    d.add(String(leftmargin + col * colwidth + 0.5 * colwidth + birthdayx, row * rowheight + birthdayy + 6, birthday[:5], fontSize = 8, fillColor = colors.black))
-    d.add(String(leftmargin + col * colwidth + 0.5 * colwidth + birthdayx, row * rowheight + birthdayy, birthday[6:10], fontSize = 8, fillColor = colors.black))
+    d.add(transform_svg("SVG/calendar-blank.svg", leftmargin + col * colwidth + 0.5 * colwidth + birthdayx, bottommargin + row * rowheight + birthdayy, 0.5 , 0.5))
+    d.add(String(leftmargin + col * colwidth + 0.5 * colwidth + birthdayx, bottommargin + row * rowheight + birthdayy + 6, birthday[:5], fontSize = 8, fillColor = colors.black))
+    d.add(String(leftmargin + col * colwidth + 0.5 * colwidth + birthdayx, bottommargin + row * rowheight + birthdayy, birthday[6:10], fontSize = 8, fillColor = colors.black))
     #land2 9
     landcode = formule1data[i][9]
-    d.add(transform_svg(lookupflag(landcode), col * colwidth + flagx, row * rowheight + flagy, 0.3 , 0.3))
-    d.add(Line(leftmargin + col * colwidth, row * rowheight + 155, col * colwidth + colwidth - 10, row * rowheight + 155, strokeColor=colors.black, strokeWidth = 1))
-    d.add(upperrightcorner(leftmargin + col * colwidth + colwidth - 14, row * rowheight + 145, 10.0, 1, colors.black))
-    d.add(Line(leftmargin + col * colwidth + colwidth - 4, row * rowheight + 145, leftmargin + col * colwidth + colwidth - 4, row * rowheight + 105, strokeColor=colors.black, strokeWidth = 1))
-    d.add(Line(leftmargin + col * colwidth, row * rowheight + 82, leftmargin + col * colwidth + 0.5 * colwidth - 8.0 - 4, row * rowheight + 82, strokeColor=colors.black, strokeWidth = 1))
-    d.add(bottomrightcorner(leftmargin + col * colwidth + 0.5 * colwidth - 8.0 - 4, row * rowheight + 90, 8.0, 1, colors.black))
-    d.add(Line(leftmargin + col * colwidth + 0.5 * colwidth - 4, row * rowheight + 90, leftmargin + col * colwidth + 0.5 * colwidth - 4, row * rowheight + 120, strokeColor=colors.black, strokeWidth = 1))
-    d.add(Line(leftmargin + col * colwidth + 0.5 * colwidth, row * rowheight + 82, leftmargin + col * colwidth + colwidth - 8.0 - 4, row * rowheight + 82, strokeColor=colors.black, strokeWidth = 1))
-    d.add(bottomrightcorner(leftmargin + col * colwidth + colwidth - 8.0 -4, row * rowheight + 90, 8.0, 1, colors.black))
-    d.add(Line(leftmargin + col * colwidth + colwidth - 4, row * rowheight + 90, leftmargin + col * colwidth + colwidth - 4, row * rowheight + 120, strokeColor=colors.black, strokeWidth = 1))
+    d.add(transform_svg(lookupflag(landcode), col * colwidth + flagx, bottommargin + row * rowheight + flagy, 0.3 , 0.3))
+    d.add(Line(leftmargin + col * colwidth, bottommargin + row * rowheight + 155, col * colwidth + colwidth - 10, bottommargin + row * rowheight + 155, strokeColor=colors.black, strokeWidth = 1))
+    d.add(upperrightcorner(leftmargin + col * colwidth + colwidth - 14, bottommargin + row * rowheight + 145, 10.0, 1, colors.black))
+    d.add(Line(leftmargin + col * colwidth + colwidth - 4, bottommargin + row * rowheight + 145, leftmargin + col * colwidth + colwidth - 4, bottommargin + row * rowheight + 105, strokeColor=colors.black, strokeWidth = 1))
+    d.add(Line(leftmargin + col * colwidth, bottommargin + row * rowheight + 82, leftmargin + col * colwidth + 0.5 * colwidth - 8.0 - 4, bottommargin + row * rowheight + 82, strokeColor=colors.black, strokeWidth = 1))
+    d.add(bottomrightcorner(leftmargin + col * colwidth + 0.5 * colwidth - 8.0 - 4, bottommargin + row * rowheight + 90, 8.0, 1, colors.black))
+    d.add(Line(leftmargin + col * colwidth + 0.5 * colwidth - 4, bottommargin + row * rowheight + 90, leftmargin + col * colwidth + 0.5 * colwidth - 4, bottommargin + row * rowheight + 120, strokeColor=colors.black, strokeWidth = 1))
+    d.add(Line(leftmargin + col * colwidth + 0.5 * colwidth, bottommargin + row * rowheight + 82, leftmargin + col * colwidth + colwidth - 8.0 - 4, bottommargin + row * rowheight + 82, strokeColor=colors.black, strokeWidth = 1))
+    d.add(bottomrightcorner(leftmargin + col * colwidth + colwidth - 8.0 -4, bottommargin + row * rowheight + 90, 8.0, 1, colors.black))
+    d.add(Line(leftmargin + col * colwidth + colwidth - 4, bottommargin + row * rowheight + 90, leftmargin + col * colwidth + colwidth - 4, bottommargin + row * rowheight + 120, strokeColor=colors.black, strokeWidth = 1))
     # logomiddle
-    d.add(Line(leftmargin + col * colwidth + logox - 0.5 * logowidth, row * rowheight + logoy, col * colwidth + logox + 0.5 * logowidth, row * rowheight + logoy, strokeColor=colors.blue, strokeWidth=1))
-    d.add(Line(col * colwidth + logox, row * rowheight + logoy - 0.5 * logoheight, col * colwidth + logox, row * rowheight + logoy + 0.5 * logoheight, strokeColor=colors.blue, strokeWidth=1))
+    d.add(Line(leftmargin + col * colwidth + logox - 0.5 * logowidth, bottommargin + row * rowheight + logoy, col * colwidth + logox + 0.5 * logowidth, bottommargin + row * rowheight + logoy, strokeColor=colors.blue, strokeWidth=1))
+    d.add(Line(col * colwidth + logox, bottommargin + row * rowheight + logoy - 0.5 * logoheight, col * colwidth + logox, bottommargin + row * rowheight + logoy + 0.5 * logoheight, strokeColor=colors.blue, strokeWidth=1))
     col = col + 1
     if col == 3:
         col = 0
