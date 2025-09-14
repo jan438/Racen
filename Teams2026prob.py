b4eecfe3e2d886025e05cbb575f3b80cb14f0f79
jan438 <jan438@users.noreply.github.com>
2025-09-14 15:20:09 +0200

    avatar circles changed

--- Teams2026prob.py
+++ Teams2026prob.py
@@ -129,7 +129,7 @@ for i in range(count):
     #land1 5
     landcode = formule1data[i][5]
     d.add(transform_svg(lookupflag(landcode), col * colwidth + 0.5 * colwidth + flagx, bottommargin + row * rowheight + flagy, 0.5 , 0.5))
-    d.add(Circle(leftmargin + col * colwidth + avatarx, bottommargin + row * rowheight + avatary, 0.5 * logowidth, strokeColor = colors.blue, strokeWidth = 1, fillColor = teamcolors2[i]))
+    d.add(Circle(leftmargin + col * colwidth + avatarx, bottommargin + row * rowheight + avatary, 0.5 * logowidth, strokeColor = None, strokeWidth = 0, fillColor = teamcolors2[i]))
     svg_root = load_svg_file("Teams/Verstappen.svg")
     svgwidth = svg_root.attrib.get('width')
     svgheight = svg_root.attrib.get('height')
@@ -142,7 +142,7 @@ for i in range(count):
     #land2 9
     landcode = formule1data[i][9]
     d.add(transform_svg(lookupflag(landcode), col * colwidth + flagx, bottommargin + row * rowheight + flagy, 0.5 , 0.5))
-    d.add(Circle(leftmargin + col * colwidth + 0.5 * colwidth + avatarx, bottommargin + row * rowheight + avatary, 0.5 * logowidth, strokeColor = colors.blue, strokeWidth = 1, fillColor = teamcolors2[i]))
+    d.add(Circle(leftmargin + col * colwidth + 0.5 * colwidth + avatarx, bottommargin + row * rowheight + avatary, 0.5 * logowidth, strokeColor = None, strokeWidth = 0, fillColor = teamcolors2[i]))
     svg_root = load_svg_file("Teams/Verstappen.svg")
     svgwidth = svg_root.attrib.get('width')
     svgheight = svg_root.attrib.get('height')
