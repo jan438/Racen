3463150877b5911a573c12600c38461187fab1aa
jan438 <jan438@users.noreply.github.com>
2025-09-15 08:42:26 +0200

    code removed

--- Teams2026prob.py
+++ Teams2026prob.py
@@ -132,7 +132,6 @@ for i in range(count):
     landcode = formule1data[i][5]
     d.add(transform_svg(lookupflag(landcode), col * colwidth + 0.5 * colwidth + flagx, bottommargin + row * rowheight + flagy, 0.5 , 0.5))
     d.add(Circle(leftmargin + col * colwidth + avatarx, bottommargin + row * rowheight + avatary, 0.5 * avatarwidth, strokeColor = None, strokeWidth = 0, fillColor = teamcolors2[i]))
-    svg_root = load_svg_file("Teams/Verstappen.svg")
     d.add(transform_svg("Teams/Verstappen.svg", leftmargin + avatarx + col * colwidth, bottommargin + avatary + row * rowheight, avatars, avatars))
     #birthday2
     birthday = formule1data[i][4]
@@ -143,7 +142,6 @@ for i in range(count):
     landcode = formule1data[i][9]
     d.add(transform_svg(lookupflag(landcode), col * colwidth + flagx, bottommargin + row * rowheight + flagy, 0.5 , 0.5))
     d.add(Circle(leftmargin + col * colwidth + 0.5 * colwidth + avatarx, bottommargin + row * rowheight + avatary, 0.5 * avatarwidth, strokeColor = None, strokeWidth = 0, fillColor = teamcolors2[i]))
-    svg_root = load_svg_file("Teams/Verstappen.svg")
     d.add(transform_svg("Teams/Verstappen.svg", leftmargin + avatarx + col * colwidth + 0.5 * colwidth, bottommargin + avatary + row * rowheight, avatars, avatars))
     d.add(Line(leftmargin + col * colwidth, bottommargin + row * rowheight + 155, col * colwidth + colwidth - 9, bottommargin + row * rowheight + 155, strokeColor=colors.black, strokeWidth = 1))
     d.add(upperrightcorner(leftmargin + col * colwidth + colwidth - 14, bottommargin + row * rowheight + 145, 10.0, 1, colors.black))
