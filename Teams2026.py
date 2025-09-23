5361eaef175ce51c111b39a6a3a003e0fa00a65c
jan438 <jan438@users.noreply.github.com>
2025-09-23 13:18:23 +0200

    alpine beurt

--- Teams2026.py
+++ Teams2026.py
@@ -159,9 +159,10 @@ for i in range(count):
     d.add(bottomrightcorner(leftmargin + col * colwidth + colwidth - 19, bottommargin + row * rowheight + 97, 0.5 * avatarwidth, 1, colors.black))
     d.add(Line(leftmargin + col * colwidth + colwidth - 4, bottommargin + row * rowheight + 97, leftmargin + col * colwidth + colwidth - 4, bottommargin + row * rowheight + 120, strokeColor=colors.black, strokeWidth = 1))
     # logomiddle
-    d.add(Line(leftmargin + col * colwidth + logox - 0.5 * logowidth, bottommargin + row * rowheight + logoy, leftmargin + col * colwidth + logox + 0.5 * logowidth, bottommargin + row * rowheight + logoy, strokeColor=colors.blue, strokeWidth=1))
-    d.add(Line(leftmargin + col * colwidth + logox, bottommargin + row * rowheight + logoy - 0.5 * logoheight, leftmargin + col * colwidth + logox, bottommargin + row * rowheight + logoy + 0.5 * logoheight, strokeColor=colors.blue, strokeWidth = 1))
-    d.add(Circle(leftmargin + col * colwidth + logox, bottommargin + row * rowheight + logoy, 0.5 * logowidth, strokeColor = colors.blue, strokeWidth = 1, fillColor = None))
+    if i == 0:
+        d.add(Line(leftmargin + col * colwidth + logox - 0.5 * logowidth, bottommargin + row * rowheight + logoy, leftmargin + col * colwidth + logox + 0.5 * logowidth, bottommargin + row * rowheight + logoy, strokeColor=colors.blue, strokeWidth=1))
+        d.add(Line(leftmargin + col * colwidth + logox, bottommargin + row * rowheight + logoy - 0.5 * logoheight, leftmargin + col * colwidth + logox, bottommargin + row * rowheight + logoy + 0.5 * logoheight, strokeColor=colors.blue, strokeWidth = 1))
+        d.add(Circle(leftmargin + col * colwidth + logox, bottommargin + row * rowheight + logoy, 0.5 * logowidth, strokeColor = colors.blue, strokeWidth = 1, fillColor = None))
     col = col + 1
     if col == 3:
         col = 0
