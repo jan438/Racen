5cc23f45234958f7ec992abc358a459c670c4652
jan438 <jan438@users.noreply.github.com>
2025-09-28 09:02:50 +0200

    set line width

--- Calendar2026LC.py
+++ Calendar2026LC.py
@@ -440,9 +440,17 @@ if rastermode:
     for i in range(24):
         # vertical lines
         for j in range(1,14):
+            if j == 5 or j == 10:
+                my_canvas.setLineWidth(1)
+            else:
+                my_canvas.setLineWidth(0.5)
             my_canvas.line(leftmargin + col * colwidth + j * 10, bottommargin + 10 + row * rowheight, leftmargin + col * colwidth + j * 10, bottommargin + 10 + (row + 1) * rowheight)
         # horizontal lines
         for j in range(1,12):
+            if j == 5 or j == 10:
+                my_canvas.setLineWidth(1)
+            else:
+                my_canvas.setLineWidth(0.5)
             my_canvas.line(leftmargin + col * colwidth, bottommargin + 10 + row * rowheight + j * 10, leftmargin + (col + 1) * colwidth, bottommargin + 10 + row * rowheight + j * 10)
         col += 1
         if col == 4:
