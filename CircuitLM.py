48c25af8b4db2d01df54e5df345082dc948a3396
jan438 <jan438@users.noreply.github.com>
2026-08-03 15:03:52 +0200

    added

--- CircuitLM.py
+++ CircuitLM.py
@@ -214,10 +214,10 @@ def GeoJSON_to_SVG(cx, geojsonfile, circuit):
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
                     path = coordinates_to_path([coords[:idx3]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
-                    #path = coordinates_to_path([coords[idx2 - 1:idx3 + 1]], scale, translate)
-                    #f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
-                    #path = coordinates_to_path([coords[idx3 - 1:]], scale, translate)
-                    #f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
+                    path = coordinates_to_path([coords[idx3:idx2]], scale, translate)
+                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec2color}"/>\n')
+                    path = coordinates_to_path([coords[idx2:idx1]], scale, translate)
+                    f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec3color}"/>\n')
                 elif cx == 6 and circuit == "Istanbul":
                     print("Turkey")
                     # idx1 68 idx2 35 idx3 86
