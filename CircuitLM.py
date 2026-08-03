a46fbc7e02d0148da4243789689374c83b551e69
jan438 <jan438@users.noreply.github.com>
2026-08-03 13:50:53 +0200

    port added

--- CircuitLM.py
+++ CircuitLM.py
@@ -278,7 +278,7 @@ def GeoJSON_to_SVG(cx, geojsonfile, circuit):
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
                     path = coordinates_to_path([coords[:idx3 + 1]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
-                elif cx == 16:
+                elif cx == 16 and circuit == "Portimão":
                     print("Portugal")
                     path = coordinates_to_path([coords[:idx3 + 1]], scale, translate)
                     f.write(f'<path d="{path}" fill="none" stroke-width="7" stroke="{sec1color}"/>\n')
@@ -368,7 +368,7 @@ with open(file_to_open, 'r') as file:
         circuitsdata.append(row)
         count += 1
 for i in range(len(circuitsdata)):
-    if circuitsdata[i][0] == "Istanbul":
+    if circuitsdata[i][0] == "Portimão":
         cx = i
         print(cx, circuitsdata[i][0])
         my_canvas = canvas.Canvas("PDF/" + circuitsdata[cx][0] + "2027LM.pdf")
