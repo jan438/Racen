a44f1282b9781d74d6ab01de86f2bcb9dd21ae23
jan438 <jan438@users.noreply.github.com>
2026-08-06 13:57:48 +0200

    Update Circuits2027LM.py

--- Circuits2027LM.py
+++ Circuits2027LM.py
@@ -241,9 +241,9 @@ for i in range(count):
     my_canvas.circle(worldlocx, worldlocy, 1.75, stroke = 0, fill = 1)
     my_canvas.circle(circuit_x + left_margin + (left_margin + colwidth - namewidth) / 2 - 22, circuit_y + bottom_margin - 9, 2.8, stroke = 0, fill = 1)
     if circuitsdata[i][9] == "a":
-        renderPDF.draw(scaleSVG("SVG/arrow-shape-turn-left.svg", clockwisescale), my_canvas, circuit_x + left_margin + namewidth + 10, circuit_y + bottom_margin - 12)
+        renderPDF.draw(scaleSVG("SVG/arrow-shape-turn-left.svg", clockwisescale), my_canvas, circuit_x + left_margin + (left_margin + colwidth - namewidth) / 2 - 22, circuit_y + bottom_margin - 12)
     else:
-        renderPDF.draw(scaleSVG("SVG/arrow-shape-turn-right.svg", clockwisescale), my_canvas, circuit_x + left_margin + namewidth + 10, circuit_y + bottom_margin - 12)
+        renderPDF.draw(scaleSVG("SVG/arrow-shape-turn-right.svg", clockwisescale), my_canvas, circuit_x + left_margin + (left_margin + colwidth - namewidth) / 2 - 22, circuit_y + bottom_margin - 12)
     legend_x = worldmap_x + 30 + legendcol * 52
     legend_y = worldmap_y - 40 + legendrow * 10
     my_canvas.circle(legend_x, legend_y, 2.8, stroke = 0, fill = 1)
