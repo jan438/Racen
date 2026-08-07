a35f143db9491b5479592673a163b99c3c9816a5
jan438 <jan438@users.noreply.github.com>
2026-08-07 08:34:38 +0200

    Update Circuits2027LM.py

--- Circuits2027LM.py
+++ Circuits2027LM.py
@@ -216,7 +216,7 @@ for i in range(count):
     my_canvas.setFont(cirfont, 9)
     my_canvas.setFillColorRGB(255,170,0)
     displayname = circuitsdata[i][24]
-    namewidth = pdfmetrics.stringWidth(displayname, "Helvetica", 9)
+    namewidth = pdfmetrics.stringWidth(displayname, cirfont, 9)
     my_canvas.drawString(circuit_x + (colwidth - namewidth) / 2, circuit_y + bottom_margin - 12, displayname)
     flag_x = startfinish_offset_x * circuitscale
     flag_y = startfinish_offset_y * circuitscale
