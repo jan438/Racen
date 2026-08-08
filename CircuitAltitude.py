32bc413e0dbed6db745bb50a9b9046081133e3c6
jan438 <jan438@users.noreply.github.com>
2026-08-08 15:10:48 +0200

    added

---
+++ CircuitAltitude.py
@@ -0,0 +1,71 @@
+import os
+import sys
+import csv
+import geojson
+import math
+from reportlab.graphics import renderPDF
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch, mm
+from reportlab.graphics.shapes import *
+from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
+
+def Altitude_to_SVG(geojsonfile, svgfile):
+    def coordinates_to_path(coordinates, scale, translate):
+        path_data = ""
+        for LineString in coordinates:
+            for i, point in enumerate(LineString):
+                x = (point[0] - translate[0]) * scale[0]
+                y = (point[1] - translate[1]) * scale[1]
+                command = "M" if i == 0 else "L"
+                path_data += f"{command}{x},{height - y} "
+        return path_data.strip()
+    with open("Data/" + geojsonfile + "A.geojson", 'r') as file:
+        geojson_data = geojson.load(file)
+    features = geojson_data['features']
+    print("Count features", len(features))
+    startindices = []
+    startfinishindex = 0
+    for feature in features:
+        geometry = feature["geometry"]
+        properties = feature['properties']
+        if geometry['type'] == 'LineString':
+            coordinates = geometry["coordinates"]
+            min_x = min_y = float('inf')
+            max_x = max_y = float('-inf')
+            coords = [coordinates]
+            for linestring in coords:
+               print(geojsonfile, "len", len(linestring))
+               for point in linestring:
+                    x, y = point
+                    min_x = min(min_x, x)
+                    max_x = max(max_x, x)
+                    min_y = min(min_y, y)
+                    max_y = max(max_y, y)
+    width = 800
+    height = 200
+    with open("SVG/" + svgfile + "A.svg", 'w') as f:
+        f.write(f'<svg width="{width}" height="{height}" xmlns="http://www.w3.org/2000/svg">\n')
+        for feature in geojson_data['features']:
+            geometry = feature['geometry']
+            coords = geometry['coordinates']
+            if geometry['type'] == 'LineString':
+                print("To write elevation")
+        f.write('</svg>')    
+    return
+if sys.platform[0] == 'l':
+    path = '/home/jan/git/Racen'
+if sys.platform[0] == 'w':
+    path = "C:/Users/janbo/OneDrive/Documents/GitHub/Racen"
+os.chdir(path)
+circuitsdata = []
+file_to_open = "Data/Circuits2027.csv"
+with open(file_to_open, 'r') as file:
+    csvreader = csv.reader(file, delimiter = ';')
+    count = 0
+    for row in csvreader:
+        circuitsdata.append(row)
+        count += 1
+circuit = "be-1925"
+Altitude_to_SVG(circuit, circuit)
+
+key = input("Wait")
