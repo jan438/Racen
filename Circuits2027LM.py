1beada8bcf56c2d77601d18f481792cfdedbd1f6
jan438 <jan438@users.noreply.github.com>
2026-08-08 14:12:25 +0200

    added

--- Circuits2027LM.py
+++ Circuits2027LM.py
@@ -234,11 +234,11 @@ for i in range(count):
     my_canvas.setFont(cirfont, 7)
     my_canvas.setFillColorRGB(170,255,127)
     lengthstr = f"{length}" + "m"
-    my_canvas.drawString(circuit_x + info_x - pdfmetrics.stringWidth(lengthstr, cirfont, 7), circuit_y + info_y + 8, lengthstr)
+    my_canvas.drawString(circuit_x + info_x - pdfmetrics.stringWidth(lengthstr, cirfont, 7) / 2, circuit_y + info_y + 8, lengthstr)
     rulerdrawing = scaleSVG("SVG/ruler.svg", rulerscale)
     renderPDF.draw(rulerdrawing, my_canvas, circuit_x + info_x - rulerdrawing.width / 2, circuit_y + info_y - rulerdrawing.height / 2)
     altitudestr = f"{altitude}" + "m"
-    my_canvas.drawString(circuit_x + info_x - pdfmetrics.stringWidth(altitudestr, cirfont, 7), circuit_y + info_y - 1, altitudestr)
+    my_canvas.drawString(circuit_x + info_x - pdfmetrics.stringWidth(altitudestr, cirfont, 7) / 2, circuit_y + info_y - 1, altitudestr)
     altitudedrawing = scaleSVG("SVG/altitude.svg", arrowscale)
     renderPDF.draw(altitudedrawing, my_canvas, circuit_x + info_x - altitudedrawing.width / 2, circuit_y + info_y - altitudedrawing.height / 2 - 10)
     worldlocx = worldmap_x + float(circuitsdata[i][3])
