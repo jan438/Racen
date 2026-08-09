b4293a6cf65fcdadf560f97ea2ad072a161c1548
jan438 <jan438@users.noreply.github.com>
2026-08-09 10:30:24 +0200

    Update request.py

--- request.py
+++ request.py
@@ -24,5 +24,4 @@ response = requests.post(
 print('Response HTTP Status Code: {status_code}'.format(status_code=response.status_code))
 print('Response HTTP Response Body: {content}'.format(content=response.content))
 
-
 key = input("Wait")
