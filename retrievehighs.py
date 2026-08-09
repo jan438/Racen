21fbc4bab4cf64210e86574f60e60eb58d4ed497
jan438 <jan438@users.noreply.github.com>
2026-08-09 15:34:45 +0200

    Update retrievehighs.py

--- retrievehighs.py
+++ retrievehighs.py
@@ -27,26 +27,28 @@ def readgeojsonfile(geojsonfile, min, max):
                         selectioncoords.append([x, y])
                     index += 1
     return selectioncoords
-
-#response = requests.post(
-#            url="https://api.open-elevation.com/api/v1/lookup",
-#            headers={
-#                "Accept": "application/json",
-#                "Content-Type": "application/json; charset=utf-8",
-#            },
-#            data=json.dumps({
-#                "locations": [
-#                    {
-#                        "longitude": 5.96502000000,
-#                        "latitude": 50.44425100000
-#                    },
-#                    {
-#                        "longitude": 5.96341900000,
-#                        "latitude": 50.44603300000
-#                    }
-#                ]
-#            })
-#        )
+    
+def lookuphighs():
+    response = requests.post(
+            url="https://api.open-elevation.com/api/v1/lookup",
+            headers={
+                "Accept": "application/json",
+                "Content-Type": "application/json; charset=utf-8",
+            },
+            data=json.dumps({
+                "locations": [
+                    {
+                        "longitude": 5.96502000000,
+                        "latitude": 50.44425100000
+                    },
+                    {
+                        "longitude": 5.96341900000,
+                        "latitude": 50.44603300000
+                    }
+                ]
+            })
+        )
+    return response
 
 #print('Response HTTP Status Code: {status_code}'.format(status_code=response.status_code))
 #print('Response HTTP Response Body: {content}'.format(content=response.content))
@@ -59,6 +61,10 @@ os.chdir(path)
 
 selectedcoords = readgeojsonfile("be-1925", 0, 10)
 print(len(selectedcoords))
+
+resp = lookuphighs()
+print('Response HTTP Status Code: {status_code}'.format(status_code=resp.status_code))
+print('Response HTTP Response Body: {content}'.format(content=resp.content))
 
 print(resp)
 
