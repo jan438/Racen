e4fdfbaeb0c142c11524f2dceddceef8f140925a
jan438 <jan438@users.noreply.github.com>
2026-08-09 14:16:10 +0200

    Update request.py

--- request.py
+++ request.py
@@ -1,27 +1,27 @@
 import requests
 import json
 
-response = requests.post(
-            url="https://api.open-elevation.com/api/v1/lookup",
-            headers={
-                "Accept": "application/json",
-                "Content-Type": "application/json; charset=utf-8",
-            },
-            data=json.dumps({
-                "locations": [
-                    {
-                        "longitude": 5.96502000000,
-                        "latitude": 50.44425100000
-                    },
-                    {
-                        "longitude": 5.96341900000,
-                        "latitude": 50.44603300000
-                    }
-                ]
-            })
-        )
+#response = requests.post(
+#            url="https://api.open-elevation.com/api/v1/lookup",
+#            headers={
+#                "Accept": "application/json",
+#                "Content-Type": "application/json; charset=utf-8",
+#            },
+#            data=json.dumps({
+#                "locations": [
+#                    {
+#                        "longitude": 5.96502000000,
+#                        "latitude": 50.44425100000
+#                    },
+#                    {
+#                        "longitude": 5.96341900000,
+#                        "latitude": 50.44603300000
+#                    }
+#                ]
+#            })
+#        )
 
-print('Response HTTP Status Code: {status_code}'.format(status_code=response.status_code))
-print('Response HTTP Response Body: {content}'.format(content=response.content))
+#print('Response HTTP Status Code: {status_code}'.format(status_code=response.status_code))
+#print('Response HTTP Response Body: {content}'.format(content=response.content))
 
 key = input("Wait")
