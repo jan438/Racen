5229e79e1a680a7bbc9f7b6ed87f645963faab3f
jan438 <jan438@users.noreply.github.com>
2026-08-11 11:17:07 +0200

    simple request added

---
+++ getRequest.py
@@ -0,0 +1,12 @@
+import requests
+
+# The API endpoint
+url = "https://jsonplaceholder.typicode.com/posts/1"
+
+# A GET request to the API
+response = requests.get(url)
+
+# Print the response
+print(response.json())
+
+key = input("Wait")
