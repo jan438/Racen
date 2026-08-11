811234ee751d5256ecbd95177b4faceee7ae9fe6
jan438 <jan438@users.noreply.github.com>
2026-08-11 14:20:57 +0200

    resp to json added

--- getRequest.py
+++ getRequest.py
@@ -1,12 +1,15 @@
 import requests
+import json
 
 url = "https://api.open-elevation.com/api/v1/lookup?locations=41.161758,-8.583933"
+response = requests.get(url)
 
-try:
-    response = requests.get(url)
-    print(response.json())
-
-except:
-	print("Exception")
-
+if response.status_code == 200:
+    new_data = response.json()
+    with open("PDF/data.json", "w") as json_file:
+        json.dump(new_data, json_file, indent=4)
+        print("Data appended to data.json file.")
+else:
+    print("Failed to retrieve data from the API. Status code:", response.status_code)
+    
 key = input("Wait")
