21bc94bb659d5e1350185f1b3dfd8dadba41677f
jan438 <jan438@users.noreply.github.com>
2026-08-11 11:28:25 +0200

    request changed open elevation added

--- getRequest.py
+++ getRequest.py
@@ -2,7 +2,7 @@ import requests
 
 # https://api.open-elevation.com/api/v1/lookup?locations=41.161758,-8.583933
 # The API endpoint
-url = "https://jsonplaceholder.typicode.com/posts/1"
+url = "https://api.open-elevation.com/api/v1/lookup?locations=41.161758,-8.583933"
 
 # A GET request to the API
 response = requests.get(url)
