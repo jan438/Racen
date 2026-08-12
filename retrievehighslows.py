70e2adca907eaed1ab67066fc9f4275e3d380d7b
jan438 <jan438@users.noreply.github.com>
2026-08-12 09:21:38 +0200

    qatar added

--- retrievehighslows.py
+++ retrievehighslows.py
@@ -44,7 +44,7 @@ if sys.platform[0] == 'w':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/Racen"
 os.chdir(path)
 
-circuitname = "us-2023"
+circuitname = "qa-2004"
 selectedcoords = readgeojsonfile(circuitname, 0, 514)
 print(len(selectedcoords))
 
