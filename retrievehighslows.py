30c69cc03f92b87688aa83b94b94483408f0557c
jan438 <jan438@users.noreply.github.com>
2026-08-12 09:51:28 +0200

    emeriates added

--- retrievehighslows.py
+++ retrievehighslows.py
@@ -44,7 +44,7 @@ if sys.platform[0] == 'w':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/Racen"
 os.chdir(path)
 
-circuitname = "us-2012"
+circuitname = "ae-2009"
 selectedcoords = readgeojsonfile(circuitname, 0, 514)
 print(len(selectedcoords))
 
