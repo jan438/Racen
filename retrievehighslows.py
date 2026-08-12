40ccef99fc3c5b27437a2cada436354a2e42c1d8
jan438 <jan438@users.noreply.github.com>
2026-08-12 09:43:28 +0200

    gb added

--- retrievehighslows.py
+++ retrievehighslows.py
@@ -44,7 +44,7 @@ if sys.platform[0] == 'w':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/Racen"
 os.chdir(path)
 
-circuitname = "cn-2004"
+circuitname = "gb-1948"
 selectedcoords = readgeojsonfile(circuitname, 0, 514)
 print(len(selectedcoords))
 
