8b522e16136bbf0780697fae51ed06461ee119fe
jan438 <jan438@users.noreply.github.com>
2026-08-12 09:31:22 +0200

    monaca added

--- retrievehighslows.py
+++ retrievehighslows.py
@@ -44,7 +44,7 @@ if sys.platform[0] == 'w':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/Racen"
 os.chdir(path)
 
-circuitname = "us-2022"
+circuitname = "mc-1929"
 selectedcoords = readgeojsonfile(circuitname, 0, 514)
 print(len(selectedcoords))
 
