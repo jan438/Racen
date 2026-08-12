851e3e2deb52a12f9a27fd1f551cb704272418c7
jan438 <jan438@users.noreply.github.com>
2026-08-12 09:23:35 +0200

    madrid added

--- retrievehighslows.py
+++ retrievehighslows.py
@@ -44,7 +44,7 @@ if sys.platform[0] == 'w':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/Racen"
 os.chdir(path)
 
-circuitname = "qa-2004"
+circuitname = "es-2026"
 selectedcoords = readgeojsonfile(circuitname, 0, 514)
 print(len(selectedcoords))
 
