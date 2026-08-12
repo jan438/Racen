ac0c5d53bd85b18f0f45a1c31c87e7946d03199f
jan438 <jan438@users.noreply.github.com>
2026-08-12 08:53:53 +0200

    bahrain added

--- retrievehighslows.py
+++ retrievehighslows.py
@@ -44,7 +44,7 @@ if sys.platform[0] == 'w':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/Racen"
 os.chdir(path)
 
-circuitname = "au-1953"
+circuitname = "bh-2002"
 selectedcoords = readgeojsonfile(circuitname, 0, 514)
 print(len(selectedcoords))
 
