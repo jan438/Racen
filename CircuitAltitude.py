c65e774cda0c0f2b1e43f61dfa960dd6d210e2a4
jan438 <jan438@users.noreply.github.com>
2026-08-13 11:26:45 +0200

    baku added

--- CircuitAltitude.py
+++ CircuitAltitude.py
@@ -44,7 +44,7 @@ if sys.platform[0] == 'l':
 if sys.platform[0] == 'w':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/Racen"
 os.chdir(path)
-circuit = "bh-2002"
+circuit = "az-2016"
 Altitude_to_SVG(circuit)
 
 key = input("Wait")
