9373bd68f1e9b62a3b146d557e69dfd3fa3d94f3
jan438 <jan438@users.noreply.github.com>
2026-08-13 14:03:12 +0200

    mina added

--- CircuitAltitude.py
+++ CircuitAltitude.py
@@ -29,7 +29,7 @@ def Altitude_to_SVG(jsonfile):
             lon = item["longitude"]
             lat = item["latitude"]
             elevation = item["elevation"]
-            a = elevation
+            a = elevation - mina 
             if i == 0:
                 path_data = f"M {l} {a}"
             else:
@@ -44,7 +44,7 @@ def Altitude_to_SVG(jsonfile):
     with open("Data/" + jsonfile + ".json", 'r') as file:
         data = json.load(file)
         path_data = coordinates_to_path(data["results"])
-        dwg = svgwrite.Drawing('SVG/' + jsonfile + 'A.svg', size=('1000px', '500px'))
+        dwg = svgwrite.Drawing('SVG/' + jsonfile + 'A.svg', size=('1000px', '200px'))
         path = dwg.path(d=path_data, fill='lightblue', stroke='blue', stroke_width=3)
         dwg.add(path)
         dwg.save()
