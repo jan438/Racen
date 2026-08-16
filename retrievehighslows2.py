4384bc170ce88ca705ef98a4c3b9289903e8c879
jan438 <jan438@users.noreply.github.com>
2026-08-16 09:37:49 +0200

    print invalid request

--- retrievehighslows2.py
+++ retrievehighslows2.py
@@ -39,6 +39,7 @@ def lookuphighs(selectedcoords):
         latitude = float(q[1])
         data["locations"].append({"latitude": latitude, "longitude": longtitude})
     response = requests.post(url, data=data)
+    print(data)
     return response
 
 if sys.platform[0] == 'l':
@@ -49,13 +50,14 @@ os.chdir(path)
 
 circuitname = "ae-2009"
 selectedcoords = readgeojsonfile(circuitname, 0, 100)
-print(len(selectedcoords))
-
+print("len selected coords", len(selectedcoords))
 resp = lookuphighs(selectedcoords)
 print('Response HTTP Status Code: {status_code}'.format(status_code=resp.status_code))
-highslows = resp.content
-data = json.loads(highslows.decode('utf-8'))
-with open('Data/' + circuitname + '-2.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
-
+if resp.status_code == 200:
+    highslows = resp.content
+    data = json.loads(highslows.decode('utf-8'))
+    with open('Data/' + circuitname + '-2.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+else:
+    print("Invalid request")
 key = input("Wait")
