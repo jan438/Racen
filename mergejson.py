e0bb7aafd74c60d54ef29aef66064782c694eea8
jan438 <jan438@users.noreply.github.com>
2026-08-16 15:40:54 +0200

    merged added

--- mergejson.py
+++ mergejson.py
@@ -4,7 +4,14 @@ import sys
 import os
 import time
 
-with open("Data/ae-2009-2-1.json", 'r') as file:
-    data = json.load(file)
+with open("Data/ae-2009-2-1.json", 'r') as file1:
+    data1 = json.load(file1)
+with open("Data/ae-2009-2-2.json", 'r') as file2:
+    data2 = json.load(file2)
+merged_data = []
+merged_data.append(data1)
+merged_data.append(data2)
+with open("Data/ae-2009-2.json", 'w') as outfile:
+    json.dump(merged_data, outfile)
 
 key = input("Wait")
