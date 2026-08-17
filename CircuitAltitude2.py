61a29d37def9b16abfd0f2c922d9b9a3e8a61b12
jan438 <jan438@users.noreply.github.com>
2026-08-17 10:14:20 +0200

    a2 added

--- CircuitAltitude2.py
+++ CircuitAltitude2.py
@@ -15,51 +15,15 @@ altitudescale = 0.5
 
 def Altitude_to_SVG(jsonfile):
     def coordinates_to_path(results):
-        mina = math.inf
-        maxa = -math.inf
-        fa = -1
-        la = -1
         for item in results:
-            elevation = item["elevation"]
-            if fa == -1:
-                fa = elevation
-            if elevation > maxa:
-                maxa = elevation
-            if elevation < mina:
-                mina = elevation
-            la = elevation
-        diff = maxa - mina
-        print("maxa", maxa, "mina", mina, "diff", diff) 
-        l = 0
+            print(item)
         path_data = ""
-        i = 0
-        for item in results:
-            location = item["location"]
-            lon = location["lng"]
-            lat = location["lat"]
-            elevation = item["elevation"]
-            a = maxa - elevation
-            a = altitudescale * a
-            if i == 0:
-                path_data = f"M {l} {a}"
-            else:
-                d = sqrt(abs(lon - plon)**2 + abs(lat - plat)**2)
-                l = l + d * lengthscale
-                path_data += f" L {l} {a}"
-            plon = lon
-            plat = lat
-            i += 1
-        mina = 100
-        fa = maxa - fa
-        path_data += f" L {l} {mina}"
-        path_data += f" L 0.0 {mina}"
-        path_data += f" L 0.0 {fa}"
-#        print(path_data)
         return path_data
-    with open("Data/" + jsonfile + ".json", 'r') as file:
+    with open("Data/" + jsonfile + "-2.json", 'r') as file:
         data = json.load(file)
+        return
         path_data = coordinates_to_path(data["results"])
-        dwg = svgwrite.Drawing('SVG/' + jsonfile + 'A.svg', size=('1000px', '300px'))
+        dwg = svgwrite.Drawing('SVG/' + jsonfile + 'A2.svg', size=('1000px', '300px'))
         grad = dwg.linearGradient(start=(0, 0), end=(0, 1), id='my-gradient')
         grad.add_stop_color(offset=0.0, color='#6d6d6d', opacity=1)
         grad.add_stop_color(offset=1.0, color='#000000', opacity=1)
