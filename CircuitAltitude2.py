988cc7c37721bb8048c5765f10e7c22ec940f574
jan438 <jan438@users.noreply.github.com>
2026-08-18 13:22:57 +0200

    bh added

--- CircuitAltitude2.py
+++ CircuitAltitude2.py
@@ -110,6 +110,6 @@ print("circuitsdata", count)
 #for j in range(count):
 #    Altitude_to_SVG(circuitsdata[j][1])
 #    print(circuitsdata[j][0])
-Altitude_to_SVG("au-1953")
+Altitude_to_SVG("bh-2002")
 
 key = input("Wait")
