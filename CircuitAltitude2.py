41fcb156c7237cf23f7b5c231fd81a6f7e3b4319
jan438 <jan438@users.noreply.github.com>
2026-08-18 10:56:56 +0200

    it added

--- CircuitAltitude2.py
+++ CircuitAltitude2.py
@@ -110,6 +110,6 @@ print("circuitsdata", count)
 #for j in range(count):
 #    Altitude_to_SVG(circuitsdata[j][1])
 #    print(circuitsdata[j][0])
-Altitude_to_SVG("mc-1929")
+Altitude_to_SVG("it-1922")
 
 key = input("Wait")
