7c197666ec46e2ce389a57e7a1576acd570a9654
jan438 <jan438@users.noreply.github.com>
2026-08-18 11:06:34 +0200

    china added

--- retrievehighslows2.py
+++ retrievehighslows2.py
@@ -50,7 +50,7 @@ if sys.platform[0] == 'w':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/Racen"
 os.chdir(path)
 
-circuitname = "at-1969"
+circuitname = "cn-2004"
 selectedcoords = readgeojsonfile(circuitname, 0, 100)
 print("len selected coords", len(selectedcoords))
 resp = lookuphighs(selectedcoords)
