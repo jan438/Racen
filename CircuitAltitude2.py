ca53b44c2c699d1080ddfa7ee0c09594f707da56
jan438 <jan438@users.noreply.github.com>
2026-08-18 09:41:15 +0200

    hungary added

--- CircuitAltitude2.py
+++ CircuitAltitude2.py
@@ -15,6 +15,7 @@ altitudescale = 0.5
 
 def Altitude_to_SVG(jsonfile):
     def coordinates_to_path(dat1, dat2):
+        path_data = ""
         total = 0
         totalcoords = []
         res1 = dat1["results"]
@@ -49,7 +50,6 @@ def Altitude_to_SVG(jsonfile):
             la = elevation
         diff = maxa - mina
         l = 0
-        path_data = ""
         for i in range(len(totalcoords)):
             elevation = totalcoords[i][0]
             lon = totalcoords[i][1]
@@ -70,23 +70,26 @@ def Altitude_to_SVG(jsonfile):
         path_data += f" L 0.0 {mina}"
         path_data += f" L 0.0 {fa}"
         return path_data
-    file1 = "Data/" + jsonfile + "-2-1.json"
-    file2 = "Data/" + jsonfile + "-2-2.json"
-    print(os.path.exists(file1), os.path.exists(file2))
-    with open("Data/" + jsonfile + "-2.json", 'r') as file:
-        data = json.load(file)
-        data1 = data[0]
-        data2 = data[1]
-        path_data = coordinates_to_path(data1, data2)
-        dwg = svgwrite.Drawing('SVG/' + jsonfile + 'A.svg', size=('1000px', '300px'))
-        grad = dwg.linearGradient(start=(0, 0), end=(0, 1), id='my-gradient')
-        grad.add_stop_color(offset=0.0, color='#6d6d6d', opacity=1)
-        grad.add_stop_color(offset=1.0, color='#000000', opacity=1)
-        dwg.defs.add(grad)
-        path = dwg.path(d=path_data, fill="url(#my-gradient)", stroke='black', stroke_width=3)
-        dwg.add(path)
-        dwg.add(dwg.text('ABC', insert=(55, 60), stroke='none', fill='#ffff7f', font_size='30px', font_weight="bold", font_family="Arial"))
-        dwg.save()
+    file1str = "Data/" + jsonfile + "-2-1.json"
+    file2str = "Data/" + jsonfile + "-2-2.json"
+    if os.path.exists(file1str):
+        with open(file1str, 'r') as file1:
+            data1 = json.load(file1)
+            if os.path.exists(file2str):
+                with open(file2str, 'r') as file2:
+                    data2 = json.load(file2)
+            else:
+                data2 = None
+            path_data = coordinates_to_path(data1, data2)
+            dwg = svgwrite.Drawing('SVG/' + jsonfile + 'A.svg', size=('1000px', '300px'))
+            grad = dwg.linearGradient(start=(0, 0), end=(0, 1), id='my-gradient')
+            grad.add_stop_color(offset=0.0, color='#6d6d6d', opacity=1)
+            grad.add_stop_color(offset=1.0, color='#000000', opacity=1)
+            dwg.defs.add(grad)
+            path = dwg.path(d=path_data, fill="url(#my-gradient)", stroke='black', stroke_width=3)
+            dwg.add(path)
+            dwg.add(dwg.text('ABC', insert=(55, 60), stroke='none', fill='#ffff7f', font_size='30px', font_weight="bold", font_family="Arial"))
+            dwg.save()
     return
 if sys.platform[0] == 'l':
     path = '/home/jan/git/Racen'
@@ -106,6 +109,6 @@ print("circuitsdata", count)
 #for j in range(count):
 #    Altitude_to_SVG(circuitsdata[j][1])
 #    print(circuitsdata[j][0])
-Altitude_to_SVG("mx-1962")
+Altitude_to_SVG("hu-1986")
 
 key = input("Wait")
