05ccf9ccec45b654f7952afe86bde2d21e6d7f27
jan438 <jan438@users.noreply.github.com>
2026-08-18 10:34:01 +0200

    marina added

--- CircuitAltitude2.py
+++ CircuitAltitude2.py
@@ -110,6 +110,6 @@ print("circuitsdata", count)
 #for j in range(count):
 #    Altitude_to_SVG(circuitsdata[j][1])
 #    print(circuitsdata[j][0])
-Altitude_to_SVG("es-2026")
+Altitude_to_SVG("sg-2008")
 
 key = input("Wait")
--- retrievehighslows2.py
+++ retrievehighslows2.py
@@ -50,7 +50,7 @@ if sys.platform[0] == 'w':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/Racen"
 os.chdir(path)
 
-circuitname = "es-2026"
+circuitname = "sg-2008"
 selectedcoords = readgeojsonfile(circuitname, 0, 100)
 print("len selected coords", len(selectedcoords))
 resp = lookuphighs(selectedcoords)
