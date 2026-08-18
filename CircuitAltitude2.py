bffc3d492ca00f7859715095cab6af7650546185
jan438 <jan438@users.noreply.github.com>
2026-08-18 11:00:10 +0200

    port added

--- CircuitAltitude2.py
+++ CircuitAltitude2.py
@@ -110,6 +110,6 @@ print("circuitsdata", count)
 #for j in range(count):
 #    Altitude_to_SVG(circuitsdata[j][1])
 #    print(circuitsdata[j][0])
-Altitude_to_SVG("it-1922")
+Altitude_to_SVG("pt-2008")
 
 key = input("Wait")
