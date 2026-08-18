840ddc17f52d8f782b70f0eb3137278001319655
jan438 <jan438@users.noreply.github.com>
2026-08-18 10:30:35 +0200

    es added

--- CircuitAltitude2.py
+++ CircuitAltitude2.py
@@ -110,6 +110,6 @@ print("circuitsdata", count)
 #for j in range(count):
 #    Altitude_to_SVG(circuitsdata[j][1])
 #    print(circuitsdata[j][0])
-Altitude_to_SVG("qa-2004")
+Altitude_to_SVG("es-2026")
 
 key = input("Wait")
--- retrievehighslows2.py
+++ retrievehighslows2.py
@@ -50,7 +50,7 @@ if sys.platform[0] == 'w':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/Racen"
 os.chdir(path)
 
-circuitname = "qa-2004"
+circuitname = "es-2026"
 selectedcoords = readgeojsonfile(circuitname, 0, 100)
 print("len selected coords", len(selectedcoords))
 resp = lookuphighs(selectedcoords)
