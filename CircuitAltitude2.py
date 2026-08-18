ec8505f6415a40fbccbdcd095a6cd19a106b120d
jan438 <jan438@users.noreply.github.com>
2026-08-18 09:47:32 +0200

    length added

--- CircuitAltitude2.py
+++ CircuitAltitude2.py
@@ -69,7 +69,7 @@ def Altitude_to_SVG(jsonfile):
         path_data += f" L {l} {mina}"
         path_data += f" L 0.0 {mina}"
         path_data += f" L 0.0 {fa}"
-        return path_data
+        return [path_data, l]
     file1str = "Data/" + jsonfile + "-2-1.json"
     file2str = "Data/" + jsonfile + "-2-2.json"
     if os.path.exists(file1str):
@@ -80,7 +80,7 @@ def Altitude_to_SVG(jsonfile):
                     data2 = json.load(file2)
             else:
                 data2 = None
-            path_data = coordinates_to_path(data1, data2)
+            [path_data, l] = coordinates_to_path(data1, data2)
             dwg = svgwrite.Drawing('SVG/' + jsonfile + 'A.svg', size=('1000px', '300px'))
             grad = dwg.linearGradient(start=(0, 0), end=(0, 1), id='my-gradient')
             grad.add_stop_color(offset=0.0, color='#6d6d6d', opacity=1)
@@ -88,7 +88,7 @@ def Altitude_to_SVG(jsonfile):
             dwg.defs.add(grad)
             path = dwg.path(d=path_data, fill="url(#my-gradient)", stroke='black', stroke_width=3)
             dwg.add(path)
-            dwg.add(dwg.text('ABC', insert=(55, 60), stroke='none', fill='#ffff7f', font_size='30px', font_weight="bold", font_family="Arial"))
+            dwg.add(dwg.text(str(l), insert=(55, 60), stroke='none', fill='#ffff7f', font_size='30px', font_weight="bold", font_family="Arial"))
             dwg.save()
     return
 if sys.platform[0] == 'l':
