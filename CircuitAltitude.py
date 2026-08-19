dbd745a24a6109c72cabdb627b18a77fef53c1bb
jan438 <jan438@users.noreply.github.com>
2026-08-19 11:40:15 +0200

    error corrected

--- CircuitAltitude.py
+++ CircuitAltitude.py
@@ -98,11 +98,11 @@ def Altitude_to_SVG(jsonfile):
             dwg.add(path)
             high = dwg.circle(center=(maxal, 0), r=10, fill='red', stroke='black', stroke_width=3)
             dwg.add(high)
-            low = dwg.circle(center=(minal, 0), r=10, fill='green', stroke='black', stroke_width=3)
+            low = dwg.circle(center=(minal, 20), r=10, fill='green', stroke='black', stroke_width=3)
             dwg.add(low)
             dwg.add(dwg.text(str(round(l, 1)), insert=(50, 60), stroke='none', fill='#ffaa00', font_size='30px', font_weight="bold", font_family="Arial"))
             dwg.add(dwg.text(str(round(maxa, 1)), insert=(maxal, 30), stroke='none', fill='#ff0000', font_size='30px', font_weight="bold", font_family="Arial"))
-            dwg.add(dwg.text(str(round(mina, 1)), insert=(minal, 30), stroke='none', fill='#00ff00', font_size='30px', font_weight="bold", font_family="Arial"))
+            dwg.add(dwg.text(str(round(mina, 1)), insert=(minal, 50), stroke='none', fill='#00ff00', font_size='30px', font_weight="bold", font_family="Arial"))
             dwg.save()
     return
 if sys.platform[0] == 'l':
