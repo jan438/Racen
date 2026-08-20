9b8c0e93501a7d08e092362088aba924c87995dc
jan438 <jan438@users.noreply.github.com>
2026-08-20 11:06:02 +0200

    90m

--- retrievehighslows.py
+++ retrievehighslows.py
@@ -50,7 +50,7 @@ if sys.platform[0] == 'w':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/Racen"
 os.chdir(path)
 
-circuitname = "az-2016"
+circuitname = "us-2012"
 selectedcoords = readgeojsonfile(circuitname, 0, 100)
 print("len selected coords", len(selectedcoords))
 resp = lookuphighs(selectedcoords)
