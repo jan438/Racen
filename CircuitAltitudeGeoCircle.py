61df5e8e372ef1c8ee60c664c9a28f72918d2be5
jan438 <jan438@users.noreply.github.com>
2026-08-25 11:01:42 +0200

    added

--- CircuitAltitudeGeoCircle.py
+++ CircuitAltitudeGeoCircle.py
@@ -37,7 +37,6 @@ def Altitude_to_SVG(jsonfile, startindex, ac):
                 lat = location["lat"]
                 coord = [elevation, lon, lat]
                 totalcoords.append(coord)
-        totalcoords = totalcoords[:10]
         lencoords = len(totalcoords)
         mina = math.inf
         maxa = -math.inf
