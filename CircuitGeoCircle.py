f7230fd405ef5ccb4595af7b68e7175b18922c8c
jan438 <jan438@users.noreply.github.com>
2026-08-28 14:47:50 +0200

    alt added

--- CircuitGeoCircle.py
+++ CircuitGeoCircle.py
@@ -136,11 +136,29 @@ def path_to_svg(jsonfile, coords, maxdifa):
     path = dwg.path(d=path_data, fill="none", stroke='green', stroke_width=10)
     dwg.add(path)
     points = [(-20, 0), (20, 0), (0, -40)]
-    dwg.add(dwg.polygon(points, fill='white', stroke='none'))
+#    dwg.add(dwg.polygon(points, fill='white', stroke='none'))
     points = [(-20, 20), (20, 20), (0, 60)]
-    dwg.add(dwg.polygon(points, fill='white', stroke='none'))
-    dwg.add(dwg.text(str(round(maxa)), insert=(20, 0), stroke='none', fill='#ffff7f', font_size='50px', font_weight="bold", font_family="Arial"))
-    dwg.add(dwg.text(str(round(mina)), insert=(20, 60), stroke='none', fill='#ffff7f', font_size='50px', font_weight="bold", font_family="Arial"))
+#    dwg.add(dwg.polygon(points, fill='white', stroke='none'))
+    maxa = round(maxa)
+    if maxa >= 1000:
+        altx = 20
+        dwg.add(dwg.text(str(maxa), insert=(altx, 0), stroke='none', fill='#ffff7f', font_size='50px', font_weight="bold", font_family="Arial"))
+    elif maxa >= 100:
+        altx = 40
+        dwg.add(dwg.text(str(maxa), insert=(altx, 0), stroke='none', fill='#ffff7f', font_size='50px', font_weight="bold", font_family="Arial"))
+    else:
+        altx = 60
+        dwg.add(dwg.text(str(maxa), insert=(altx, 0), stroke='none', fill='#ffff7f', font_size='50px', font_weight="bold", font_family="Arial"))
+    mina = round(mina)
+    if mina >= 1000:
+         altx = 20
+         dwg.add(dwg.text(str(mina), insert=(altx, 60), stroke='none', fill='#ffff7f', font_size='50px', font_weight="bold", font_family="Arial"))
+    elif mina >= 100:
+        altx = 40
+        dwg.add(dwg.text(str(mina), insert=(altx, 60), stroke='none', fill='#ffff7f', font_size='50px', font_weight="bold", font_family="Arial"))
+    else:
+        altx = 60
+        dwg.add(dwg.text(str(mina), insert=(altx, 60), stroke='none', fill='#ffff7f', font_size='50px', font_weight="bold", font_family="Arial"))
     high = dwg.circle(center=(maxal, 0), r=7, fill='red', stroke='none')
     dwg.add(high)
     low = dwg.circle(center=(minal, 0), r=7, fill='blue', stroke='none')
