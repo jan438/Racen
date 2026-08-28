9eec47d6339bf3161fd7362da525ec701d204249
jan438 <jan438@users.noreply.github.com>
2026-08-28 13:38:11 +0200

    added

--- length.py
+++ length.py
@@ -72,9 +72,10 @@ def path_length(coords):
         gdesic += geodesic(coord1, coord2).km
         d = great_circle(coord1, coord2).km
         gcircle += d
-#        print(i, lat1, lon1, lat2, lon2, d, gcircle)
     lat1, lon1 = coords[len(coords) - 1]    
     lat2, lon2 = coords[0]
+    coord1 = (lon1, lat1)
+    coord2 = (lon2, lat2)
     d = great_circle(coord1, coord2).km
     gcircle += d
 #    print(len(coords) - 1, lat1, lon1, lat2, lon2, d, gcircle)
@@ -99,10 +100,8 @@ if __name__ == "__main__":
 #        if circuitsdata[i][1] == "mc-1929":
         if True:
             coordinates = readjson(circuitsdata[i][1])
-#            coordinates = coordinates[:10]
             try:
                 [haver, l, gdesic, gcircle] = path_length(coordinates)
-#               print(f'{circuitsdata[i][0]}, haver, {haver:.3f}, math, {l:.3f}, gdesic, {gdesic:.3f}, gcircle, {gcircle:.3f}')
                 print(f'{circuitsdata[i][0]}, {circuitsdata[i][1]}, len coordinates, {len(coordinates)}, gcircle, {gcircle:.3f}')
             except Exception as e:
                 print(f"Error calculating path length: {e}")
