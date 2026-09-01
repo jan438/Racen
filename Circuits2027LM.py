4cc3590e97b22b03dc199c73a1b6aade8d640586
jan438 <jan438@users.noreply.github.com>
2026-09-01 11:33:31 +0200

    comments added

--- Circuits2027LM.py
+++ Circuits2027LM.py
@@ -59,10 +59,10 @@ def scalecolorSVG(svgfile, scaling_factor, color):
     svg_root = load_svg_file(svgfile)
     fill = svg_root.get("fill")
     print("fill attr")
-    attrib = svg_root.attrib
-    for name, value in attrib.items():
-        if name == "fill":
-            print('{0}="{1}"'.format(name, value))
+#    attrib = svg_root.attrib
+#    for name, value in attrib.items():
+#        if name == "fill":
+#            print('{0}="{1}"'.format(name, value))
     svgRenderer = SvgRenderer(svgfile)
     drawing = svgRenderer.render(svg_root)
     scaling_x = scaling_factor
