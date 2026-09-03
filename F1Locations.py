4577db4f90f26cbc61a3a4869b87bba670ec1140
jan438 <jan438@users.noreply.github.com>
2026-09-03 14:04:34 +0200

    added

---
+++ F1Locations.py
@@ -0,0 +1,39 @@
+import json
+import os
+import sys
+import csv
+import svgwrite
+import math
+from geopy.distance import great_circle
+
+circuitsdata = []
+maxdifa = -1
+
+def readjson(jsonfile):
+    totalcoords = []
+    def coordinates_to_array(dat):
+        totalcoords = []
+        res = dat["results"]
+        for item in res1:
+            location = item["location"]
+            lon = location["lng"]
+            lat = location["lat"]
+            coord = [lon, lat, alt]
+            totalcoords.append(coord)
+        return totalcoords
+    if os.path.exists(jsonfile):
+        with open(jsonfile, 'r') as file:
+            data = json.load(file)
+#            totalcoords = coordinates_to_array(data)
+    return totalcoords
+
+if __name__ == "__main__":
+    if sys.platform[0] == 'l':
+        path = '/home/jan/git/Racen'
+    if sys.platform[0] == 'w':
+        path = "C:/Users/janbo/OneDrive/Documents/GitHub/Racen"
+    os.chdir(path)
+    jsonfile = "Data/f1-locations.json"
+    coordinates = readjson(jsonfile)
+
+key = input("Wait")
