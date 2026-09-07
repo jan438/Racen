3e8406813b0820b84e81c5aa9e624445b9fbaa2a
jan438 <jan438@users.noreply.github.com>
2026-09-07 10:39:42 +0200

    Update Circuits2027LM.py

--- Circuits2027LM.py
+++ Circuits2027LM.py
@@ -45,7 +45,7 @@ cirfont = "LiberationSerif"
 
 circuitcolors = ["#fcff5d","#7dfc00","#0ec434","#228c68","#8ad8e8","#235b54","#29bdab","#3998f5",
 "#277da7","#3750db","#f22020","#991919","#ffcba5","#e68f66","#c56133","#96341c","#632819","#ffc413",
-"#f47a22","#2f2aa0","#b732cc","#772b9d","#f07cab","#d30b94","#edeff3","#c3a5b4","#946aa2","#5d4c86"]
+"#f47a22","#2f2aa0","#b732cc","#772b9d","#f07cab","#d30b94","#c3a5b4","#946aa2","#5d4c86"]
 
 def scaleSVG(svgfile, scaling_factor):
     svg_root = load_svg_file(svgfile)
