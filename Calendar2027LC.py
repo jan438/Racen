c22b0f898aa23557663224c20aec6a13d3407e09
jan438 <jan438@users.noreply.github.com>
2026-09-15 15:07:13 +0200

    Update Calendar2027LC.py

--- Calendar2027LC.py
+++ Calendar2027LC.py
@@ -222,8 +222,6 @@ my_canvas.drawString(100, 775, "2027 Calendar " + version)
 row = 6
 col = 0
 for i in range(len(raceevents)):
-    if i == 12:
-        break
     raceevent = raceevents[i]
     partindex = summary.find(" (")
     event = summary[:partindex]
