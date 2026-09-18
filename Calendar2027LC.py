d9d67443d1107fd91f0a754451881a8cf332e7a5
jan438 <jan438@users.noreply.github.com>
2026-09-18 15:17:07 +0200

    added

--- Calendar2027LC.py
+++ Calendar2027LC.py
@@ -231,7 +231,7 @@ for i in range(len(raceevents)):
     if event == "Race":
         stateindex = raceevent.summary.find("Race (Grand Prix of ")
         state = raceevent.summary[stateindex + 20:len(raceevent.summary) - 1]
-        if i == 1 or i == 2 or i == 3 or i == 4 or i == 5 or i == 6 or i == 7 or i == 8 or i == 9 or i == 10 or i == 11 or i == 12 or i == 13 or i == 14 or i == 15 or i == 17 or i == 18 or i == 19 or i == 20 or i == 21 or i == 23 or i == 24:
+        if i == 1 or i == 2 or i == 3 or i == 4 or i == 5 or i == 6 or i == 7 or i == 8 or i == 9 or i == 10 or i == 11 or i == 12 or i == 13 or i == 14 or i == 15 or i == 16 or i == 17 or i == 18 or i == 19 or i == 20 or i == 21 or i == 23 or i == 24:
             print(state)
         else:
             state = "Belgium"
