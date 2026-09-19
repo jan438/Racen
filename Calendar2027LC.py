287af70fbc7673cee5c6bc6523e12a9f0dec93f9
jan438 <jan438@users.noreply.github.com>
2026-09-19 09:35:26 +0200

    added

--- Calendar2027LC.py
+++ Calendar2027LC.py
@@ -231,11 +231,6 @@ for i in range(len(raceevents)):
     if event == "Race":
         stateindex = raceevent.summary.find("Race (Grand Prix of ")
         state = raceevent.summary[stateindex + 20:len(raceevent.summary) - 1]
-        if i == 1 or i == 2 or i == 3 or i == 4 or i == 5 or i == 6 or i == 7 or i == 8 or i == 9 or i == 10 or i == 11 or i == 12 or i == 13 or i == 14 or i == 15 or i == 16 or i == 17 or i == 18 or i == 19 or i == 20 or i == 21 or i == 22 or i == 23 or i == 24:
-            print(state)
-        else:
-            state = "Belgium"
-            print("else", state)
         drawing = scaleSVG('Location/' + state + '.svg', 0.1)
         renderPDF.draw(drawing, my_canvas, col * colwidth + leftmargin + 50, (row - 1) * rowheight + bottommargin + 50)
         my_canvas.drawString(col * colwidth + leftmargin + 20, (row - 1) * rowheight + bottommargin + 20, raceevents[i].location)
