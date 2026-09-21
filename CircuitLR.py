0805caed493a84ea8067abc7b3a481e0dabaa4e2
jan438 <jan438@users.noreply.github.com>
2026-09-21 15:51:39 +0200

    Update CircuitLR.py

--- CircuitLR.py
+++ CircuitLR.py
@@ -25,10 +25,8 @@ def scaleSVG(svgfile, scaling_factor):
     drawing.scale(scaling_x, scaling_y)
     return drawing
 def SVG_to_RSVG(svgfile):
-    root = etree.Element("svg")
-    svg = etree.tostring(root,pretty_print=True)
-    print(svg)
     tree = etree.parse('Location/Belgium.svg')
+    svg = tree.xpath('//*[local-name()="svg"]')[0]
     id = tree.xpath('//*[local-name()="svg"]//*[local-name()="g"]/*[local-name()="path"]/@id')[0]
     d = tree.xpath('//*[local-name()="svg"]//*[local-name()="g"]/*[local-name()="path"]/@d')[0]
     f = tree.xpath('//*[local-name()="svg"]//*[local-name()="g"]/*[local-name()="path"]/@fill')[0]
