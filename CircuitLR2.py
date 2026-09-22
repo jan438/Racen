20ece900b3e5c77adb64d58f5171dab6f477fcdf
jan438 <jan438@users.noreply.github.com>
2026-09-22 08:59:02 +0200

    added

--- CircuitLR2.py
+++ CircuitLR2.py
@@ -34,7 +34,7 @@ def SVG_to_RSVG(svgfile):
         print("Created new <defs> element.")
     else:
         print("Found existing <defs> element.")
-    radial_gradient = etree.SubElement(defs, f"{{{SVG_NS}}}radialGradient", id="grad1")
+    radial_gradient = etree.SubElement(defs, f"{{{SVG_NS}}}radialGradient", id="grad1", cx="80%", cy="60%")
     etree.SubElement(radial_gradient, f"{{{SVG_NS}}}stop", offset="0%", style="stop-color:#b1b100;stop-opacity:1")
     etree.SubElement(radial_gradient, f"{{{SVG_NS}}}stop", offset="100%", style="stop-color:#555500;stop-opacity:1")
     ns = {"svg": "http://www.w3.org/2000/svg"}
