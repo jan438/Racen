c7405092bf63d69bb97f33b6e26013796a4ba249
jan438 <jan438@users.noreply.github.com>
2026-09-24 14:31:25 +0200

    Update Calendar2027LC.py

--- Calendar2027LC.py
+++ Calendar2027LC.py
@@ -249,6 +249,8 @@ for i in range(len(raceevents)):
         renderPDF.draw(drawing, my_canvas, col * colwidth + leftmargin + 4, (row - 1) * rowheight + bottommargin + 50)
         drawing = scaleSVG('Clocks/1530om.svg', clockscale)
         renderPDF.draw(drawing, my_canvas, col * colwidth + leftmargin + 14, (row - 1) * rowheight + bottommargin + 50)
+        drawing = scaleSVG('SVG/PortimãoLC.svg', sc)
+        renderPDF.draw(drawing, my_canvas, col * colwidth + leftmargin + 4, (row - 1) * rowheight + bottommargin + 50)
         col += 1
         if col == 4:
             col = 0
