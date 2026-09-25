d22c82bd96e3222daddb00864da50d85a95a3f04
jan438 <jan438@users.noreply.github.com>
2026-09-25 13:25:27 +0200

    added

--- CircuitLC.py
+++ CircuitLC.py
@@ -164,15 +164,16 @@ drawing = svg2rlg('SVG/F1.svg')
 renderPDF.draw(drawing, my_canvas, 300, 750)
 name_x = 300
 name_y = 25
-cx = 60
-cy = 50
-[offset_x, offset_y] = GeoJSON_to_SVG(circuitsdata[i][1], circuitsdata[i][0], cx, cy)
-circuit_x = 0
-circuit_y = 0
-renderPDF.draw(scaleSVG("SVG/" + circuitsdata[i][0] + "LC.svg", circuitscale), my_canvas, circuit_x + left_margin, circuit_y + bottom_margin)
-my_canvas.drawString(circuit_x + left_margin + name_x, circuit_y + bottom_margin + name_y, circuitsdata[i][0])
-flag_x = offset_x * circuitscale
-flag_y = offset_y * circuitscale
-renderPDF.draw(scaleSVG("SVG/finishflag.svg", circuitscale), my_canvas, circuit_x + left_margin + flag_x + flagcorrection * circuitscale, circuit_y + bottom_margin + flag_y)
+for i in range(count):
+    cx = 60
+    cy = 50
+    [offset_x, offset_y] = GeoJSON_to_SVG(circuitsdata[i][1], circuitsdata[i][0], cx, cy)
+    circuit_x = 0
+    circuit_y = 0
+    renderPDF.draw(scaleSVG("SVG/" + circuitsdata[i][0] + "LC.svg", circuitscale), my_canvas, circuit_x + left_margin, circuit_y + bottom_margin)
+    my_canvas.drawString(circuit_x + left_margin + name_x, circuit_y + bottom_margin + name_y, circuitsdata[i][0])
+    flag_x = offset_x * circuitscale
+    flag_y = offset_y * circuitscale
+    renderPDF.draw(scaleSVG("SVG/finishflag.svg", circuitscale), my_canvas, circuit_x + left_margin + flag_x + flagcorrection * circuitscale, circuit_y + bottom_margin + flag_y)
 my_canvas.save()
 key = input("Wait")
