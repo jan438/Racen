e13a1ebb266c7900ef88d78ceddbfe07ef0ec9fe
jan438 <jan438@users.noreply.github.com>
2026-09-25 13:54:37 +0200

    Update Calendar2027LC.py

--- Calendar2027LC.py
+++ Calendar2027LC.py
@@ -248,7 +248,7 @@ for i in range(len(raceevents)):
         drawing = scaleSVG('SVG/calendar.svg', calscale)
         renderPDF.draw(drawing, my_canvas, col * colwidth + leftmargin, (row - 1) * rowheight + bottommargin + 50)
         drawing = scaleSVG('Clocks/1530om.svg', clockscale)
-        renderPDF.draw(drawing, my_canvas, col * colwidth + leftmargin + 16, (row - 1) * rowheight + bottommargin + 50)
+        renderPDF.draw(drawing, my_canvas, col * colwidth + leftmargin + 20, (row - 1) * rowheight + bottommargin + 50)
         drawing = scaleSVG('SVG/' + circuitsdata[cx][0] + 'LC.svg', circuitscale)
         renderPDF.draw(drawing, my_canvas, col * colwidth + leftmargin + 90, (row - 1) * rowheight + bottommargin + 50)
         col += 1
